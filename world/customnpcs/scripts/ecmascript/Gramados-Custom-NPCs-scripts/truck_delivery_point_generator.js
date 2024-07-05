84b025a0b4bd5edde2b4fa96173dcd9e6b82d666
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2024-07-05 17:30:31 +0200

    added the first step of truck driver job

---
+++ world/customnpcs/scripts/ecmascript/Gramados-Custom-NPCs-scripts/truck_delivery_point_generator.js
@@ -0,0 +1,440 @@
+/*
+This script is for plans to add the truck driver job on the Gramados server.
+The final goal is to have several saved destinations, and a GUI to select one of several random delivery (from a point to another).
+Each delivery will also fill the truck with a random cargo, and the player will have to deliver it to the destination.
+
+This script is used to create a new point that will be stored in world data to be chosen from for the quest creation script.
+*/
+
+// GUI
+var GUI, GRID_H = 16, GRID_W = 16, GRID_BORDER = 2;
+
+// file path for the destinations
+var FILE_PATH = "world/customnpcs/scripts/truck_destinations.json";
+var destinations = [];
+
+// Constant IDs for buttons and text fields
+var BUTTON_REGION_PREV = 301,
+    BUTTON_REGION_NEXT = 302,
+    BUTTON_CATEGORY_PREV = 311,
+    BUTTON_CATEGORY_NEXT = 312,
+    BUTTON_PRODUCER_PREV = 321,
+    BUTTON_PRODUCER_NEXT = 322,
+    BUTTON_CANCEL = 13,
+    BUTTON_CONFIRM = 14,
+    ENTRY_ID = 101,
+    ENTRY_NAME = 102,
+    ENTRY_X = 103,
+    ENTRY_Y = 104,
+    ENTRY_Z = 105,
+    ENTRY_QUANTITY_FACTOR = 106,
+    ENTRY_DESCRIPTION = 200,
+    TEXT_REGION = 351,
+    TEXT_CATEGORY = 352,
+    TEXT_PRODUCER = 353,
+    LIST_TYPE = 100;
+
+
+//list
+var scrollSelection = [];
+
+// currently created point data
+var new_wip_destination = {
+    name: "",
+    id: 0,
+    x: 0,
+    y: 0,
+    z: 0,
+    description: "",
+    types: [],
+    category: "",
+    region: "",
+    trade_type: "",
+    quantity_factor: 40
+};
+
+// list of available regions
+var regions = ["Gramados", "Greenfield", "Monsalac", "Farmiston", "Allenis"];
+
+// dictionary of available categories
+var categories = {
+    "Material": ["wood", "stone", "dirt", "sand", "gravel", "clay", "terracotta", "iron", "steel", "scrap metal"],
+    "Fluid": ["water", "lava", "milk", "oil", "ethanol", "diesel", "biodiesel", "concrete", "biomass"],
+    "Food": ["grain", "vegetables", "fruits", "dairy", "cheese", "seafood", "twingo juice"],
+    "Organic": ["flowers", "saplings", "seeds"],
+    "Animals": ["Angus cow", "Friesian cow", "Hereford cow", "Holstein cow", "Longhorn cow", "Highland cow", "Jersey cow"],
+    "Armoury": ["ammo", "missiles", "grenades", "rockets", "handguns", "rifles", "shotguns", "sniper rifles", "machine guns", "rocket launchers"],
+};
+
+// list of available trade types
+var trade_types = ["Producer", "Consumer"];
+
+// Script for scripted item to generate a point on interact
+function interact(event) {
+
+    // Load the destinations from the json
+    destinations = load_destinations(event);
+    for (var i = 0; i < destinations.length; i++) {
+        event.player.message("Destination " + i + ": " + destinations[i].name);
+    }
+
+    GUI = event.API.createCustomGui(1, GRID_W * 14, GRID_H * 11, false);
+
+    create_GUI(event, GRID_W, GRID_H);
+
+    event.player.showCustomGui(GUI);
+
+    return true;
+}
+
+function create_GUI(event, GRID_W, GRID_H) {
+
+    GUI.setBackgroundTexture("minecraft:textures/gui/gramados_trucker_point_gui.png");
+
+    var button_texture_sheet = "minecraft:textures/gui/gramados_trucker_point_putton_textures.png";
+
+    // Name entry
+    GUI.addLabel(16, "Name", GRID_W + GRID_BORDER, GRID_H, (GRID_W * 2) - (2 * GRID_BORDER), GRID_H);
+    GUI.addTextField(ENTRY_NAME, GRID_BORDER + GRID_W * 3, GRID_H + GRID_BORDER, GRID_W * 4 - 2 * GRID_BORDER, GRID_H - 2*GRID_BORDER);
+
+    // ID entry
+    GUI.addLabel(17, "ID", GRID_W + GRID_BORDER, GRID_H * 2, GRID_W * 2, GRID_H);
+    GUI.addTextField(ENTRY_ID, GRID_W * 3  + GRID_BORDER, GRID_H * 2  + GRID_BORDER, GRID_W * 4  - 2 * GRID_BORDER, GRID_H  - 2 * GRID_BORDER);
+
+    // X entry
+    GUI.addLabel(18, "X", GRID_W + GRID_BORDER, GRID_H * 3, GRID_W * 2, GRID_H);
+    GUI.addTextField(ENTRY_X, GRID_W + GRID_BORDER, GRID_H * 4 + GRID_BORDER, GRID_W * 2 - 2 * GRID_BORDER, GRID_H - 2 * GRID_BORDER);
+
+    // Y entry
+    GUI.addLabel(19, "Y", GRID_W * 3 + GRID_BORDER, GRID_H * 3, GRID_W * 2, GRID_H);
+    GUI.addTextField(ENTRY_Y, GRID_W * 3 + GRID_BORDER, GRID_H * 4 + GRID_BORDER, GRID_W * 2 - 2 * GRID_BORDER, GRID_H - 2 * GRID_BORDER);
+
+    // Z entry
+    GUI.addLabel(20, "Z", GRID_W * 5 + GRID_BORDER, GRID_H * 3, GRID_W * 2, GRID_H);
+    GUI.addTextField(ENTRY_Z, GRID_W * 5 + GRID_BORDER, GRID_H * 4 + GRID_BORDER, GRID_W * 2 - 2 * GRID_BORDER, GRID_H - 2 * GRID_BORDER);
+
+    // Description entry
+    GUI.addLabel(21, "Description", GRID_W + GRID_BORDER, GRID_H * 5, GRID_W * 6, GRID_H);
+    GUI.addTextField(ENTRY_DESCRIPTION, GRID_W + GRID_BORDER, GRID_H * 6 + GRID_BORDER, GRID_W * 6 - 2 * GRID_BORDER, GRID_H - 2 * GRID_BORDER);
+
+    // Consumer / Producer
+    GUI.addTexturedButton(BUTTON_PRODUCER_PREV, "", GRID_W, GRID_H * 7, GRID_W, GRID_H, button_texture_sheet, GRID_W * 8, 0);
+    GUI.addLabel(TEXT_PRODUCER, "Producer", GRID_W * 2 + GRID_BORDER, GRID_H * 7, GRID_W * 4, GRID_H);
+    GUI.addTexturedButton(BUTTON_PRODUCER_NEXT, "", GRID_W * 6, GRID_H * 7, GRID_W, GRID_H, button_texture_sheet, GRID_W * 7, 0);
+
+    // Quantity Factor
+    GUI.addLabel(22, "Quantity", GRID_W + GRID_BORDER, GRID_H * 8, GRID_W * 3, GRID_H);
+    GUI.addTextField(ENTRY_QUANTITY_FACTOR, GRID_W * 4 + GRID_BORDER, GRID_H * 8 + GRID_BORDER, GRID_W * 3 - 2 * GRID_BORDER, GRID_H - 2 * GRID_BORDER);
+
+
+    // Region label
+    GUI.addLabel(23, "Region", GRID_W * 7 + GRID_BORDER, GRID_H, GRID_W * 3, GRID_H);
+    GUI.addTexturedButton(BUTTON_REGION_PREV, "", GRID_W * 7, GRID_H * 2, GRID_W, GRID_H, button_texture_sheet, GRID_W * 8, 0);
+    GUI.addLabel(TEXT_REGION, "", GRID_W * 8 + GRID_BORDER, GRID_H * 2, GRID_W * 4, GRID_H);
+    GUI.addTexturedButton(BUTTON_REGION_NEXT, "", GRID_W * 12, GRID_H * 2, GRID_W, GRID_H, button_texture_sheet, GRID_W * 7, 0);
+
+    // Category label
+    GUI.addLabel(24, "Category", GRID_W * 7 + GRID_BORDER, GRID_H * 3, GRID_W * 3, GRID_H);
+    GUI.addTexturedButton(BUTTON_CATEGORY_PREV, "", GRID_W * 7, GRID_H * 4, GRID_W, GRID_H, button_texture_sheet, GRID_W * 8, 0);
+    GUI.addLabel(TEXT_CATEGORY, "", GRID_W * 8 + GRID_BORDER, GRID_H * 4, GRID_W * 4, GRID_H);
+    GUI.addTexturedButton(BUTTON_CATEGORY_NEXT, "", GRID_W * 12, GRID_H * 4, GRID_W, GRID_H, button_texture_sheet, GRID_W * 7, 0);
+
+    // Type list
+    GUI.addLabel(25, "Type(s)", GRID_W * 7 + GRID_BORDER, GRID_H * 5, GRID_W * 3, GRID_H);
+    GUI.addScroll(LIST_TYPE, GRID_W * 7 + GRID_BORDER, GRID_H * 6, GRID_W * 6 - GRID_BORDER * 2, GRID_H * 3, get_type_list()).setMultiSelect(true);
+
+    // CANCEL button
+    GUI.addTexturedButton(BUTTON_CANCEL, "Cancel", GRID_W, GRID_H * 9, GRID_W * 6, GRID_H, button_texture_sheet, 0, GRID_H * 6 - 1);
+    // CONFIRM button
+    GUI.addTexturedButton(BUTTON_CONFIRM, "Confirm", GRID_W * 7, GRID_H * 9, GRID_W * 6, GRID_H, button_texture_sheet, 0, GRID_H * 3 - 1);
+
+    // Prefill the GUI with the current point data
+    prefill_GUI(event.player);
+}
+
+
+function customGuiButton(event) {
+    var b1 = event.buttonId;
+
+    switch (b1) {
+        // Cancel
+        case BUTTON_CANCEL:
+            event.player.closeGui();
+            break;
+        // Confirm
+        case BUTTON_CONFIRM:
+            save_GUI_content(event);
+            event.player.closeGui();
+            break;
+        // Region previous
+        case BUTTON_REGION_PREV:
+            var region = GUI.getComponent(TEXT_REGION).getText();
+            var index = regions.indexOf(region);
+            if (index > 0) {
+                GUI.getComponent(TEXT_REGION).setText(regions[index - 1]);
+            }
+            update_type_list();
+            break;
+        // Region next
+        case BUTTON_REGION_NEXT:
+            var region = GUI.getComponent(TEXT_REGION).getText();
+            var index = regions.indexOf(region);
+            if (index < regions.length - 1) {
+                GUI.getComponent(TEXT_REGION).setText(regions[index + 1]);
+            }
+            update_type_list();
+            break;
+        // Category previous
+        case BUTTON_CATEGORY_PREV:
+            var category = GUI.getComponent(TEXT_CATEGORY).getText();
+            var index = Object.keys(categories).indexOf(category);
+            if (index > 0) {
+                GUI.getComponent(TEXT_CATEGORY).setText(Object.keys(categories)[index - 1]);
+            }
+            update_type_list();
+            break;
+        // Category next
+        case BUTTON_CATEGORY_NEXT:
+            var category = GUI.getComponent(TEXT_CATEGORY).getText();
+            var index = Object.keys(categories).indexOf(category);
+            if (index < Object.keys(categories).length - 1) {
+                GUI.getComponent(TEXT_CATEGORY).setText(Object.keys(categories)[index + 1]);
+            }
+            update_type_list();
+            break;
+        // trade type previous
+        case BUTTON_PRODUCER_PREV:
+            var trade_type = GUI.getComponent(TEXT_PRODUCER).getText();
+            var index = trade_types.indexOf(trade_type);
+            if (index > 0) {
+                GUI.getComponent(TEXT_PRODUCER).setText(trade_types[index - 1]);
+            } else {
+                GUI.getComponent(TEXT_PRODUCER).setText(trade_types[trade_types.length - 1]);
+            }
+            update_type_list();
+            break;
+        // trade type next
+        case BUTTON_PRODUCER_NEXT:
+            var trade_type = GUI.getComponent(TEXT_PRODUCER).getText();
+            var index = trade_types.indexOf(trade_type);
+            if (index < trade_types.length - 1) {
+                GUI.getComponent(TEXT_PRODUCER).setText(trade_types[index + 1]);
+            } else {
+                GUI.getComponent(TEXT_PRODUCER).setText(trade_types[0]);
+            }
+            update_type_list();
+            break
+    }
+
+    event.gui.update(event.player);
+}
+
+
+function customGuiScroll(event) {
+    scrollSelection = event.selection;
+}
+
+function save_GUI_content(event) {
+    var name = event.gui.getComponent(ENTRY_NAME).getText();
+    var id = event.gui.getComponent(ENTRY_ID).getText();
+    var x = event.gui.getComponent(ENTRY_X).getText();
+    var y = event.gui.getComponent(ENTRY_Y).getText();
+    var z = event.gui.getComponent(ENTRY_Z).getText();
+    var description = event.gui.getComponent(ENTRY_DESCRIPTION).getText();
+    var region = event.gui.getComponent(TEXT_REGION).getText();
+    var category = event.gui.getComponent(TEXT_CATEGORY).getText();
+    var trade_type = event.gui.getComponent(TEXT_PRODUCER).getText();
+    var quantity_factor = event.gui.getComponent(ENTRY_QUANTITY_FACTOR).getText();
+    var types = [];
+    for (var i = 0; i < scrollSelection.length; i++) {
+        types.push(scrollSelection[i]);
+        event.player.message("Type: " + scrollSelection[i]);
+    }
+
+    new_wip_destination = {
+        name: name,
+        id: id,
+        x: x,
+        y: y,
+        z: z,
+        description: description,
+        region: region,
+        category: category,
+        types: types,
+        trade_type: trade_type,
+        quantity_factor: quantity_factor
+    };
+
+    display_delivry_chat(event);
+
+    event.player.message("Existing destinations: " + destinations.length);
+    add_destination(new_wip_destination);
+}
+
+
+function prefill_GUI(player) {
+
+    GUI.getComponent(ENTRY_NAME).setText(new_wip_destination.name);
+
+    // if id is "", then get a new id
+    if (new_wip_destination.id == "") {
+        new_wip_destination.id = get_available_id()
+    }
+    GUI.getComponent(ENTRY_ID).setText(new_wip_destination.id);
+
+    // use the player position
+    new_wip_destination.x = player.getX();
+    new_wip_destination.y = player.getY();
+    new_wip_destination.z = player.getZ();
+    GUI.getComponent(ENTRY_X).setText(Math.round(new_wip_destination.x));
+    GUI.getComponent(ENTRY_Y).setText(Math.round(new_wip_destination.y));
+    GUI.getComponent(ENTRY_Z).setText(Math.round(new_wip_destination.z));
+
+    GUI.getComponent(ENTRY_DESCRIPTION).setText(new_wip_destination.description);
+
+    if (new_wip_destination.region == "") {
+        new_wip_destination.region = regions[0];
+    }
+    GUI.getComponent(TEXT_REGION).setText(new_wip_destination.region);
+
+    if (new_wip_destination.category == "") {
+        new_wip_destination.category = Object.keys(categories)[0];
+    }
+    GUI.getComponent(TEXT_CATEGORY).setText(new_wip_destination.category);
+
+    if (new_wip_destination.types == "") {
+        new_wip_destination.types = categories[new_wip_destination.category];
+    }
+
+    if (new_wip_destination.trade_type == "") {
+        new_wip_destination.trade_type = trade_types[0];
+    }
+    GUI.getComponent(TEXT_PRODUCER).setText(new_wip_destination.trade_type);
+
+    GUI.getComponent(ENTRY_QUANTITY_FACTOR).setText(new_wip_destination.quantity_factor);
+
+    update_type_list();
+
+}
+
+function update_type_list() {
+    // get the types compatible with the category
+    var category = GUI.getComponent(TEXT_CATEGORY).getText();
+    var types = categories[category];
+
+    // remove old list
+    GUI.removeComponent(LIST_TYPE);
+    // add new list
+    GUI.addScroll(LIST_TYPE, GRID_W * 7, GRID_H * 6, GRID_W * 6, GRID_H * 3, types).setMultiSelect(true);
+}
+
+
+function get_available_id() {
+    var id = 0;
+    for (var i = 0; i < destinations.length; i++) {
+        if (destinations[i].id == id) {
+            id++;
+        }
+    }
+    return id;
+}
+
+function get_type_list() {
+    var list = [];
+    for (var key in categories) {
+        //concatenate the list
+        list = list.concat(categories[key]);
+    }
+    return list;
+}
+
+function display_delivry_chat(event) {
+    event.player.message("Name: " + new_wip_destination.name);
+    event.player.message("ID: " + new_wip_destination.id);
+    event.player.message("X: " + new_wip_destination.x);
+    event.player.message("Y: " + new_wip_destination.y);
+    event.player.message("Z: " + new_wip_destination.z);
+    event.player.message("Description: " + new_wip_destination.description);
+    event.player.message("Region: " + new_wip_destination.region);
+    event.player.message("Category: " + new_wip_destination.category);
+    event.player.message("Types: ");
+    for (var i = 0; i < new_wip_destination.types.length; i++) {
+        event.player.message(new_wip_destination.types[i]);
+    }
+
+    return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Function to load the destinations from world data
+function load_destinations(event) {
+    // load the destinations
+    if (!file_exists()) {
+        create_file();
+        event.player.message("File created");
+    }
+    var data = read_file();
+    var destinations = JSON.parse(data);
+    event.player.message("Loaded destinations: " + destinations.length);
+    return destinations;
+}
+
+// Function to read the file
+function read_file() {
+    var ips = new java.io.FileInputStream(FILE_PATH);
+    var fileReader = new java.io.InputStreamReader(ips, "UTF-8");
+    var data1 = fileReader.read();
+    var data;
+    var start1 = "";
+    while (data1 != -1) {
+        data = String.fromCharCode(data1);
+        start1 = start1 + data;
+        data1 = fileReader.read();
+    }
+    return start1;
+}
+
+// Function to create data
+function create_file() {
+    var fileWriter = new java.io.FileWriter(FILE_PATH);
+    fileWriter.close();
+}
+
+// Function to check if file exists
+function file_exists() {
+    var file = new java.io.File(FILE_PATH);
+    return file.exists();
+}
+
+// Function to add a destination to the json
+function add_destination(destination) {
+    if (destinations == "") {
+        destinations = [];
+    }
+    destinations.push(destination);
+    create_data();
+}
+
+// Function to create data
+function create_data() {
+    var fileWriter = new java.io.FileWriter(FILE_PATH);
+    fileWriter.write(JSON.stringify(destinations));
+    fileWriter.close();
+}
---
+++ world/customnpcs/scripts/ecmascript/Gramados-Custom-NPCs-scripts/truck_delivery_quests.js
@@ -0,0 +1,251 @@
+/*
+This script is for plans to add the truck driver job on the Gramados server.
+The final goal is to have several saved destinations, and a GUI to select one of several random delivery (from a point to another).
+Each delivery will also fill the truck with a random cargo, and the player will have to deliver it to the destination.
+*/
+
+//get player
+var player;
+
+var destinations = [];
+
+var FILE_PATH = "world/customnpcs/scripts/truck_destinations.json";
+var PHONE_FILE_PATH = "world/customnpcs/scripts/players_phone.json";
+
+
+
+// list of available regions
+var regions = ["Gramados", "Greenfield", "Monsalac", "Farmiston", "Allenis"];
+
+// dictionary of available categories
+var categories = {
+    "Material": ["wood", "stone", "dirt", "sand", "gravel", "clay", "terracotta", "iron", "steel", "scrap metal"],
+    "Fluid": ["water", "lava", "milk", "oil", "ethanol", "diesel", "biodiesel", "concrete", "biomass"],
+    "Food": ["grain", "vegetables", "fruits", "dairy", "cheese", "seafood", "twingo juice"],
+    "Organic": ["flowers", "saplings", "seeds"],
+    "Animals": ["Angus cow", "Friesian cow", "Hereford cow", "Holstein cow", "Longhorn cow", "Highland cow", "Jersey cow"],
+    "Armoury": ["ammo", "missiles", "grenades", "rockets", "handguns", "rifles", "shotguns", "sniper rifles", "machine guns", "rocket launchers"],
+};
+
+// list of available trade types
+var trade_types = ["Producer", "Consumer"];
+
+var valid_cargo = [];
+
+
+function tick(event) {
+    player = event.player;
+    return true;
+}
+
+// Script for scripted block to generate a quest on interact
+function interact(event) {
+    player = event.player;
+    var world = player.world;
+
+    // load the destinations
+    if (!file_exists(FILE_PATH)) {
+        create_data(FILE_PATH);
+    }
+    
+    destinations = load_destinations();
+
+    list_valid_cargos();
+
+    // create a new delivery quest
+    create_delivery_quest(world);
+
+    return true;
+}
+
+function create_delivery_quest(world) {
+    // From the list of valid cargos, we select one randomly
+    var cargo = valid_cargo[Math.floor(Math.random() * valid_cargo.length)];
+    var cargo_name = cargo.name;
+    var cargo_category = cargo.category;
+    var cargo_producer = cargo.producer_list[Math.floor(Math.random() * cargo.producer_list.length)];
+    var cargo_consumer = cargo.consumer_list[Math.floor(Math.random() * cargo.consumer_list.length)];
+
+    // Get a quantity of cargo (basec on teh smallest quantity of the producer and consumer)
+    var cargo_quantity = Math.min(cargo_producer.quantity_factor, cargo_consumer.quantity_factor);
+    // make the quantity random between 0.5 and 1 of the quantity
+    cargo_quantity = Math.floor(cargo_quantity * (Math.random() * 0.5 + 0.5));
+    // multiply by 9
+    cargo_quantity = cargo_quantity * 9;
+
+    world.broadcast("New Truck Delivery Quest: Deliver " + cargo_quantity + " stacks of " + cargo_name + " from " + cargo_producer.name + " to " + cargo_consumer.name + ".");
+
+    save_truck_job_on_phone();
+}
+
+function list_valid_cargos() {
+    var tested_cargo
+    // For all the categories and their type, we loop through all the destinations to check if there is at least one producer and one consumer of the cargo.
+    for (var category in categories) {
+        for (var i = 0; i < categories[category].length; i++) {
+            var cargo = categories[category][i];
+            tested_cargo = {
+                "name": cargo,
+                "category": category,
+                "producer_list": [],
+                "consumer_list": []
+            };
+            for (var j = 0; j < destinations.length; j++) {
+                for (var look_type_it = 0; look_type_it < destinations[j].types.length; look_type_it++) {
+                    if (destinations[j].trade_type == "Producer" && destinations[j].types[look_type_it] == cargo) {
+                        tested_cargo.producer_list.push(destinations[j]);
+                    }
+                    if (destinations[j].trade_type == "Consumer" && destinations[j].types[look_type_it] == cargo) {
+                        tested_cargo.consumer_list.push(destinations[j]);
+                    }
+                }
+            }
+            if (tested_cargo.producer_list.length > 0 && tested_cargo.consumer_list.length > 0) {
+                valid_cargo.push(tested_cargo);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+// function to store the quest into player's phone
+function save_truck_job_on_phone() {
+
+    // get player's nickname
+    var player_name = player.getName();
+
+    // check if the player has phone data
+    //(look for player name key)
+    player.message("Player name: " + player_name);
+
+    // get the player's phone data
+    var player_phone_data = check_user(player_name);
+
+    player.message("Player phone data: " + player_phone_data);
+}
+
+
+
+
+
+
+
+// Function to load the destinations from world data
+function load_destinations() {
+    // load the destinations
+    if (!file_exists(FILE_PATH)) {
+        create_file(FILE_PATH);
+    }
+    var data = read_file(FILE_PATH);
+    var destinations = JSON.parse(data);
+    return destinations;
+}
+
+// Function to create data
+function create_file(path) {
+    var fileWriter = new java.io.FileWriter(path);
+    fileWriter.close();
+}
+
+// Function to check if file exists
+function file_exists(path) {
+    var file = new java.io.File(path);
+    return file.exists();
+}
+
+// Function to add a destination to the json
+function add_destination(destination) {
+    if (destinations == "") {
+        destinations = [];
+    }
+    destinations.push(destination);
+    create_data();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// Function to read the file
+function read_file(path) {
+    var ips = new java.io.FileInputStream(path);
+    var fileReader = new java.io.InputStreamReader(ips, "UTF-8");
+    var data1 = fileReader.read();
+    var data;
+    var start1 = "";
+    while (data1 != -1) {
+        data = String.fromCharCode(data1);
+        start1 = start1 + data;
+        data1 = fileReader.read();
+    }
+    return start1;
+}
+
+// Function to write data to the file
+function create_data(path, data) {
+    var fileWriter = new java.io.FileWriter(path);
+    fileWriter.write(JSON.stringify(data));
+    fileWriter.close();
+}
+
+
+// function to add a new user to the phone
+function check_user(player_name) {
+    var data = read_file(PHONE_FILE_PATH);
+    var phone_data = JSON.parse(data);
+
+    // check if the player has phone data
+    //(look for player name key)
+    if (phone_data[player_name] == undefined) {
+        phone_data[player_name] = [];
+        player.message("It seems you don't have a phone yet. Creating new data.");
+
+        // add a json entry with player name and UUID
+        var new_player = {
+            "name": player_name,
+            "uuid": player.getUUID(),
+            "apps": []
+        };
+
+        // add the player to the phone data
+        phone_data[player_name] = new_player;
+    }
+
+    // Save the data in the file
+    create_data(PHONE_FILE_PATH, phone_data);
+
+    return phone_data[player_name];
+}
+
+// Function to check if an app is installed
+function check_app_installed(player_phone_data, app_name, install_if_not) {
+
+    if (install_if_not == undefined) {
+        install_if_not = false;
+    }
+
+    var apps = player_phone_data.apps;
+    for (var i = 0; i < apps.length; i++) {
+        if (apps[i].name == app_name) {
+            return true;
+        }
+    }
+    if (install_if_not) {
+        apps.push(app_name);
+        create_data(PHONE_FILE_PATH, player_phone_data);
+    }
+    return false;
+}
