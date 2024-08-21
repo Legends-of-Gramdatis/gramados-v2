88e4bc164a8de7c5e826f2d43ded18bfe69ffbc1
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2024-08-22 00:17:52 +0200

    reads updated data

--- world/customnpcs/scripts/ecmascript/Gramados-Custom-NPCs-scripts/truck_delivery_point_generator.js
+++ world/customnpcs/scripts/ecmascript/Gramados-Custom-NPCs-scripts/truck_delivery_point_generator.js
@@ -10,7 +10,8 @@ This script is used to create a new point that will be stored in world data to b
 var GUI, GRID_H = 16, GRID_W = 16, GRID_BORDER = 2;
 
 // file path for the destinations
-var FILE_PATH = "world/customnpcs/scripts/truck_destinations.json";
+//var FILE_PATH = "world/customnpcs/scripts/truck_destinations.json";
+var FILE_PATH_NEW = "world/customnpcs/scripts/trucker_job_data.json";
 var destinations = [];
 
 // Constant IDs for buttons and text fields
@@ -22,6 +23,8 @@ var BUTTON_REGION_PREV = 301,
     BUTTON_PRODUCER_NEXT = 322,
     BUTTON_QUANTITY_PREV = 331,
     BUTTON_QUANTITY_NEXT = 332,
+    BUTTON_DIFFICULTY_PREV = 341,
+    BUTTON_DIFFICULTY_NEXT = 342,
     BUTTON_CANCEL = 13,
     BUTTON_CONFIRM = 14,
     ENTRY_ID = 101,
@@ -29,11 +32,13 @@ var BUTTON_REGION_PREV = 301,
     ENTRY_X = 103,
     ENTRY_Y = 104,
     ENTRY_Z = 105,
+    ENTRY_WHIGHT = 106,
     ENTRY_DESCRIPTION = 200,
-    TEXT_REGION = 351,
-    TEXT_CATEGORY = 352,
-    TEXT_PRODUCER = 353,
-    IMAGE_QUANTITY = 354,
+    TEXT_REGION = 451,
+    TEXT_CATEGORY = 452,
+    TEXT_PRODUCER = 453,
+    TEXT_DIFFICULTY = 454,
+    IMAGE_QUANTITY = 455,
     LIST_TYPE = 100;
 
 
@@ -42,17 +47,20 @@ var scrollSelection = [];
 
 // currently created point data
 var new_wip_destination = {
+    key: "",
     name: "",
-    id: 0,
+    description: "",
+    region: "",
     x: 0,
     y: 0,
     z: 0,
-    description: "",
-    types: [],
+    access_difficulty: "",
+
     category: "",
-    region: "",
     trade_type: "",
-    quantity_factor: 40
+    types: [],
+    quantity_factor: 16,
+    wheight: 10
 };
 
 // list of available regions
@@ -71,6 +79,9 @@ var categories = {
 // list of available trade types
 var trade_types = ["Producer", "Consumer"];
 
+// list of access difficulty
+var access_difficulty = ["Easy", "Medium", "Hard"];
+
 // List of quantities
 var quantities = [16, 32, 48, 96, 144];
 var current_quantity = 16;
@@ -88,17 +99,23 @@ function init(event) {
 // Script for scripted item to generate a point on interact
 function interact(event) {
 
-    // Load the destinations from the json
-    destinations = load_destinations(event);
-    for (var i = 0; i < destinations.length; i++) {
-        event.player.message("Destination " + i + ": " + destinations[i].name);
-    }
-
     GUI = event.API.createCustomGui(1, GRID_W * 14, GRID_H * 11, false);
 
     create_GUI(event, GRID_W, GRID_H);
 
     event.player.showCustomGui(GUI);
+
+    var data = load_data(event, FILE_PATH_NEW);
+
+    /*event.player.message("Loaded data: " + JSON.stringify(data));
+    event.player.message("Loaded contents: There are " + data.contents.regions.length + " regions");
+
+    // count the number of destinations (keys in "data.destinations")
+    var count = 0;
+    for (var key in data.destinations) {
+        count++;
+    }
+    event.player.message("There are " + count + " destinations");*/
 
     return true;
 }
@@ -111,13 +128,13 @@ function create_GUI(event, GRID_W, GRID_H) {
 
     // Name entry
     GUI.addLabel(16, "Name", GRID_W + GRID_BORDER, GRID_H, (GRID_W * 2) - (2 * GRID_BORDER), GRID_H);
-    GUI.addTextField(ENTRY_NAME, GRID_BORDER + GRID_W * 3, GRID_H + GRID_BORDER, GRID_W * 4 - 2 * GRID_BORDER, GRID_H - 2*GRID_BORDER);
+    GUI.addTextField(ENTRY_NAME, GRID_BORDER + GRID_W * 3, GRID_H + GRID_BORDER, GRID_W * 4 - 2 * GRID_BORDER, GRID_H - 2 * GRID_BORDER);
     // hover text:
     GUI.getComponent(ENTRY_NAME).setHoverText("Name of the destination");
 
     // ID entry
     GUI.addLabel(17, "ID", GRID_W + GRID_BORDER, GRID_H * 2, GRID_W * 2, GRID_H);
-    GUI.addTextField(ENTRY_ID, GRID_W * 3  + GRID_BORDER, GRID_H * 2  + GRID_BORDER, GRID_W * 4  - 2 * GRID_BORDER, GRID_H  - 2 * GRID_BORDER);
+    GUI.addTextField(ENTRY_ID, GRID_W * 3 + GRID_BORDER, GRID_H * 2 + GRID_BORDER, GRID_W * 4 - 2 * GRID_BORDER, GRID_H - 2 * GRID_BORDER);
     // hover text:
     GUI.getComponent(ENTRY_ID).setHoverText("ID of the destination");
 
@@ -136,44 +153,53 @@ function create_GUI(event, GRID_W, GRID_H) {
     // Description entry
     GUI.addLabel(21, "Description", GRID_W + GRID_BORDER, GRID_H * 5, GRID_W * 6, GRID_H);
     GUI.addTextField(ENTRY_DESCRIPTION, GRID_W + GRID_BORDER, GRID_H * 6 + GRID_BORDER, GRID_W * 6 - 2 * GRID_BORDER, GRID_H - 2 * GRID_BORDER);
-    // hover text:
     GUI.getComponent(ENTRY_DESCRIPTION).setHoverText("Description of the destination");
 
     // Consumer / Producer
     GUI.addTexturedButton(BUTTON_PRODUCER_PREV, "", GRID_W, GRID_H * 7, GRID_W, GRID_H, button_texture_sheet, GRID_W * 8, 0);
     GUI.addLabel(TEXT_PRODUCER, "Producer", GRID_W * 2 + GRID_BORDER, GRID_H * 7, GRID_W * 4, GRID_H);
     GUI.addTexturedButton(BUTTON_PRODUCER_NEXT, "", GRID_W * 6, GRID_H * 7, GRID_W, GRID_H, button_texture_sheet, GRID_W * 7, 0);
-    // hover text:
     GUI.getComponent(TEXT_PRODUCER).setHoverText("Weather the destination is a producer or a consumer");
-    
+
 
     // Quantity selection
     GUI.addTexturedButton(BUTTON_QUANTITY_PREV, "", GRID_W, GRID_H * 8, GRID_W, GRID_H, button_texture_sheet, GRID_W * 8, 0);
     GUI.addTexturedRect(IMAGE_QUANTITY, button_texture_sheet, GRID_W * 2, GRID_H * 8, GRID_W * 4, GRID_H, 0, GRID_H * (9 + quantities.indexOf(current_quantity)));
     GUI.addTexturedButton(BUTTON_QUANTITY_NEXT, "", GRID_W * 6, GRID_H * 8, GRID_W, GRID_H, button_texture_sheet, GRID_W * 7, 0);
-    // hover text:
     GUI.getComponent(IMAGE_QUANTITY).setHoverText("Quantity of the cargo, refer to the icons. Current quantity: Up to " + current_quantity * 9 + " stacks / buckets");
 
 
-    // Region label
-    GUI.addLabel(23, "Region", GRID_W * 7 + GRID_BORDER, GRID_H, GRID_W * 3, GRID_H);
-    GUI.addTexturedButton(BUTTON_REGION_PREV, "", GRID_W * 7, GRID_H * 2, GRID_W, GRID_H, button_texture_sheet, GRID_W * 8, 0);
-    GUI.addLabel(TEXT_REGION, "", GRID_W * 8 + GRID_BORDER, GRID_H * 2, GRID_W * 4, GRID_H);
-    GUI.addTexturedButton(BUTTON_REGION_NEXT, "", GRID_W * 12, GRID_H * 2, GRID_W, GRID_H, button_texture_sheet, GRID_W * 7, 0);
-    // hover text:
+    // Region selection
+    GUI.addTexturedButton(BUTTON_REGION_PREV, "", GRID_W * 7, GRID_H, GRID_W, GRID_H, button_texture_sheet, GRID_W * 8, 0);
+    GUI.addLabel(TEXT_REGION, "", GRID_W * 8 + GRID_BORDER, GRID_H, GRID_W * 4, GRID_H);
+    GUI.addTexturedButton(BUTTON_REGION_NEXT, "", GRID_W * 12, GRID_H, GRID_W, GRID_H, button_texture_sheet, GRID_W * 7, 0);
     GUI.getComponent(TEXT_REGION).setHoverText("Select the Island region where the destination is located");
 
-    // Category label
-    GUI.addLabel(24, "Category", GRID_W * 7 + GRID_BORDER, GRID_H * 3, GRID_W * 3, GRID_H);
-    GUI.addTexturedButton(BUTTON_CATEGORY_PREV, "", GRID_W * 7, GRID_H * 4, GRID_W, GRID_H, button_texture_sheet, GRID_W * 8, 0);
-    GUI.addLabel(TEXT_CATEGORY, "", GRID_W * 8 + GRID_BORDER, GRID_H * 4, GRID_W * 4, GRID_H);
-    GUI.addTexturedButton(BUTTON_CATEGORY_NEXT, "", GRID_W * 12, GRID_H * 4, GRID_W, GRID_H, button_texture_sheet, GRID_W * 7, 0);
+    // Category selection
+    GUI.addTexturedButton(BUTTON_CATEGORY_PREV, "", GRID_W * 7, GRID_H * 2, GRID_W, GRID_H, button_texture_sheet, GRID_W * 8, 0);
+    GUI.addLabel(TEXT_CATEGORY, "", GRID_W * 8 + GRID_BORDER, GRID_H * 2, GRID_W * 4, GRID_H);
+    GUI.addTexturedButton(BUTTON_CATEGORY_NEXT, "", GRID_W * 12, GRID_H * 2, GRID_W, GRID_H, button_texture_sheet, GRID_W * 7, 0);
+    GUI.getComponent(TEXT_CATEGORY).setHoverText("Select the category of cargo that can be delivered to or from the destination");
+
+    // Difficulty selection
+    GUI.addTexturedButton(BUTTON_DIFFICULTY_PREV, "", GRID_W * 7, GRID_H * 3, GRID_W, GRID_H, button_texture_sheet, GRID_W * 8, 0);
+    GUI.addLabel(TEXT_DIFFICULTY, "", GRID_W * 8 + GRID_BORDER, GRID_H * 3, GRID_W * 4, GRID_H);
+    GUI.addTexturedButton(BUTTON_DIFFICULTY_NEXT, "", GRID_W * 12, GRID_H * 3, GRID_W, GRID_H, button_texture_sheet, GRID_W * 7, 0);
+    GUI.getComponent(TEXT_DIFFICULTY).setHoverText("Select the access difficulty of the destination. Stuff like the type of road, the room to maneuver, etc. are taken into account.");
+
+    
 
     // Type list
-    GUI.addLabel(25, "Type(s)", GRID_W * 7 + GRID_BORDER, GRID_H * 5, GRID_W * 3, GRID_H);
-    GUI.addScroll(LIST_TYPE, GRID_W * 7 + GRID_BORDER, GRID_H * 6, GRID_W * 6 - GRID_BORDER * 2, GRID_H * 3, get_type_list()).setMultiSelect(true);
+    GUI.addLabel(25, "Type(s)", GRID_W * 7 + GRID_BORDER, GRID_H * 4, GRID_W * 3, GRID_H);
+    GUI.addScroll(LIST_TYPE, GRID_W * 7 + GRID_BORDER, GRID_H * 5, GRID_W * 6 - GRID_BORDER * 2, GRID_H * 3, get_type_list()).setMultiSelect(true);
     // hover text:
     GUI.getComponent(25).setHoverText("Select the type(s) of cargo that can be delivered to the destination. Warning: fill this part last, as it is reset when changing the category and other.");
+
+    // Wheight selection
+    GUI.addLabel(22, "Wheight", GRID_W * 7 + GRID_BORDER, GRID_H * 8, GRID_W * 3, GRID_H);
+    GUI.addTextField(ENTRY_WHIGHT, GRID_W * 10 + GRID_BORDER, GRID_H * 8 + GRID_BORDER, GRID_W * 3 - 2 * GRID_BORDER, GRID_H - 2 * GRID_BORDER);
+    GUI.getComponent(ENTRY_WHIGHT).setHoverText("The chance of these cargos to be selected for a delivery. The higher the wheight, the higher the chance.");
+
 
     // CANCEL button
     GUI.addTexturedButton(BUTTON_CANCEL, "Cancel", GRID_W, GRID_H * 9, GRID_W * 6, GRID_H, button_texture_sheet, 0, GRID_H * 6 - 1);
@@ -282,6 +308,26 @@ function customGuiButton(event) {
             GUI.getComponent(IMAGE_QUANTITY).setHoverText("Quantity of the cargo, refer to the icons. Current quantity: Up to " + current_quantity * 9 + " stacks / buckets");
             update_type_list();
             break;
+        // Difficulty previous
+        case BUTTON_DIFFICULTY_PREV:
+            var difficulty = GUI.getComponent(TEXT_DIFFICULTY).getText();
+            var index = access_difficulty.indexOf(difficulty);
+            if (index > 0) {
+                GUI.getComponent(TEXT_DIFFICULTY).setText(access_difficulty[index - 1]);
+            }
+            update_type_list();
+            break;
+        // Difficulty next
+        case BUTTON_DIFFICULTY_NEXT:
+            var difficulty = GUI.getComponent(TEXT_DIFFICULTY).getText();
+            var index = access_difficulty.indexOf(difficulty);
+            if (index < access_difficulty.length - 1) {
+                GUI.getComponent(TEXT_DIFFICULTY).setText(access_difficulty[index + 1]);
+            }
+            update_type_list();
+            break;
+
+
     }
 
     event.gui.update(event.player);
@@ -302,6 +348,9 @@ function save_GUI_content(event) {
     var region = event.gui.getComponent(TEXT_REGION).getText();
     var category = event.gui.getComponent(TEXT_CATEGORY).getText();
     var trade_type = event.gui.getComponent(TEXT_PRODUCER).getText();
+    var access_difficulty = event.gui.getComponent(TEXT_DIFFICULTY).getText();
+    var wheight = event.gui.getComponent(ENTRY_WHIGHT).getText();
+
     var types = [];
     for (var i = 0; i < scrollSelection.length; i++) {
         types.push(scrollSelection[i]);
@@ -309,17 +358,20 @@ function save_GUI_content(event) {
     }
 
     new_wip_destination = {
-        name: name,
-        id: id,
+        key: id,
+        display_name: name,
+        description: description,
+        region: region,
         x: x,
         y: y,
         z: z,
-        description: description,
-        region: region,
+        access_difficulty: access_difficulty,
+
         category: category,
-        types: types,
         trade_type: trade_type,
-        quantity_factor: String(current_quantity)
+        types: types,
+        quantity_factor: current_quantity,
+        wheight: wheight
     };
 
     display_delivry_chat(event);
@@ -349,6 +401,11 @@ function prefill_GUI(player) {
 
     GUI.getComponent(ENTRY_DESCRIPTION).setText(new_wip_destination.description);
 
+    if (new_wip_destination.wheight == "") {
+        new_wip_destination.wheight = 10;
+    }
+    GUI.getComponent(ENTRY_WHIGHT).setText(new_wip_destination.wheight);
+
     if (new_wip_destination.region == "") {
         new_wip_destination.region = regions[0];
     }
@@ -358,6 +415,12 @@ function prefill_GUI(player) {
         new_wip_destination.category = Object.keys(categories)[0];
     }
     GUI.getComponent(TEXT_CATEGORY).setText(new_wip_destination.category);
+
+    if (new_wip_destination.access_difficulty == "") {
+        new_wip_destination.access_difficulty = access_difficulty[0];
+    }
+    GUI.getComponent(TEXT_DIFFICULTY).setText(new_wip_destination.access_difficulty);
+
 
     if (new_wip_destination.types == "") {
         new_wip_destination.types = categories[new_wip_destination.category];
@@ -382,7 +445,7 @@ function update_type_list() {
     // remove old list
     GUI.removeComponent(LIST_TYPE);
     // add new list
-    GUI.addScroll(LIST_TYPE, GRID_W * 7, GRID_H * 6, GRID_W * 6, GRID_H * 3, types).setMultiSelect(true);
+    GUI.addScroll(LIST_TYPE, GRID_W * 7, GRID_H * 5, GRID_W * 6, GRID_H * 3, types).setMultiSelect(true);
 }
 
 
@@ -413,6 +476,9 @@ function display_delivry_chat(event) {
     event.player.message("Z: " + new_wip_destination.z);
     event.player.message("Description: " + new_wip_destination.description);
     event.player.message("Region: " + new_wip_destination.region);
+    event.player.message("Trade type: " + new_wip_destination.trade_type);
+    event.player.message("Quantity factor: " + new_wip_destination.quantity_factor);
+    event.player.message("Access difficulty: " + new_wip_destination.access_difficulty);
     event.player.message("Category: " + new_wip_destination.category);
     event.player.message("Types: ");
     for (var i = 0; i < new_wip_destination.types.length; i++) {
@@ -439,59 +505,52 @@ function display_delivry_chat(event) {
 
 
 
-
-// Function to load the destinations from world data
-function load_destinations(event) {
-    // load the destinations
-    if (!file_exists()) {
-        create_file();
-        event.player.message("File created");
-    }
-    var data = read_file();
-    var destinations = JSON.parse(data);
-    event.player.message("Loaded destinations: " + destinations.length);
-    return destinations;
-}
-
-// Function to read the file
-function read_file() {
-    var ips = new java.io.FileInputStream(FILE_PATH);
-    var fileReader = new java.io.InputStreamReader(ips, "UTF-8");
-    var data1 = fileReader.read();
-    var data;
-    var start1 = "";
-    while (data1 != -1) {
-        data = String.fromCharCode(data1);
-        start1 = start1 + data;
-        data1 = fileReader.read();
-    }
-    return start1;
-}
-
-// Function to create data
-function create_file() {
-    var fileWriter = new java.io.FileWriter(FILE_PATH);
-    fileWriter.close();
-}
-
-// Function to check if file exists
-function file_exists() {
-    var file = new java.io.File(FILE_PATH);
-    return file.exists();
-}
-
 // Function to add a destination to the json
-function add_destination(destination) {
-    if (destinations == "") {
+function save_data(data, file_path) {
+    /*if (destinations == "") {
         destinations = [];
     }
     destinations.push(destination);
-    create_data();
+    create_data();*/
 }
 
-// Function to create data
-function create_data() {
-    var fileWriter = new java.io.FileWriter(FILE_PATH);
-    fileWriter.write(JSON.stringify(destinations));
+// function to check if a file exists
+function check_file_exists(file_path) {
+    var file = new java.io.File(file_path);
+    return file.exists();
+}
+// function to create a file
+function create_json_file(file_path) {
+    var fileWriter = new java.io.FileWriter(file_path);
+    fileWriter.write("{}");
     fileWriter.close();
+}
+
+
+// Function to load data
+function load_data(event, data_file_path) {
+    // Check if the file exists, and create it if it doesn't
+    if (!check_file_exists(data_file_path)) {
+        create_json_file(data_file_path);
+        event.player.message("File created");
+    } else {
+        event.player.message("File exists");
+    }
+
+    var ips = new java.io.FileInputStream(data_file_path);
+    var fileReader = new java.io.InputStreamReader(ips, "UTF-8");
+    var readFile = fileReader.read();
+    var data;
+    var start = "";
+    while (readFile != -1) {
+        data = String.fromCharCode(readFile);
+        start = start + data;
+        readFile = fileReader.read();
+    }
+
+    var json_data = JSON.parse(start);
+
+    //event.player.message("Loaded data: " + JSON.stringify(json_data));
+
+    return json_data;
 }
