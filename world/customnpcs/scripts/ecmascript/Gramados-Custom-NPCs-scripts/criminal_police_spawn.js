7129a32b44e325c89c7ba5ed5326f1fc8c855a53
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2024-08-25 22:53:40 +0200

    added criminal swatting

--- world/customnpcs/scripts/ecmascript/Gramados-Custom-NPCs-scripts/criminal_police_spawn.js
+++ world/customnpcs/scripts/ecmascript/Gramados-Custom-NPCs-scripts/criminal_police_spawn.js
@@ -3,7 +3,14 @@
 var API = Java.type('noppes.npcs.api.NpcAPI').Instance()
 
 var counter = 0;
-var max_counter = 1000;
+var max_counter = 10000;
+
+var arrested_player_name = "";
+
+function init(event) {
+    // set a random counter based on the current time
+    counter = Math.floor(Math.random() * max_counter);
+}
 
 function tick(event) {
 
@@ -20,43 +27,117 @@ function isFriendOfCriminals(event) {
     var player = event.player
     var world = player.world;
     // check if the player is friendly to Criminal
-    if (player.getFactionPoints(6) > 1800)
-    {
+    if (player.getFactionPoints(6) > 1800) {
         attempt_arrrest(event, player, world);
+        arrested_player_name = player.getDisplayName();
     }
 }
 
 function attempt_arrrest(event, player, world) {
+    player.message("&4You have been arrested by the police or SPO for your criminal activities! You shall perish in the name of the law!");
     //var name = player.getDisplayName();
-    setup_police_arrest(event, player, world);
+    //setup_police_arrest(event, player, world);
 
-    // switch between the different types of arrest
-    // if criminality is between 1800 and 2000, spawn a police arrest
-    // if criminality is between 2000 and 2500, spawn 2 police arrests
-    // if criminality is between 2500 and 3000, spawn a police arrest and a SPO arrest
-    // if criminality is between 3000 and 4000, spawn 2 SPO arrests
-    // if criminality is between 4000 and 5000, spawn 4 SPO arrests
-    // if criminality is between 5000 and 6000, spawn 8 SPO arrests
+    var success = false;
+
+    if (player.getFactionPoints(6) >= 1800 && player.getFactionPoints(6) < 2000) {
+        success = spawn_arrest(event, player, world, "police", Math.floor(Math.random() * 3) + 2, 10, 5);
+    } else if (player.getFactionPoints(6) >= 2000 && player.getFactionPoints(6) < 2500) {
+        success = spawn_arrest(event, player, world, "police", Math.floor(Math.random() * 3) + 2, 10, 5);
+        success = success || spawn_arrest(event, player, world, "police", Math.floor(Math.random() * 3) + 2, 10, 5);
+    } else if (player.getFactionPoints(6) >= 2500 && player.getFactionPoints(6) < 3000) {
+        success = spawn_arrest(event, player, world, "police", Math.floor(Math.random() * 3) + 2, 10, 5);
+        success = success || spawn_spo_fireteam(event, player, world);
+    } else if (player.getFactionPoints(6) >= 3000 && player.getFactionPoints(6) < 4000) {
+        success = spawn_spo_fireteam(event, player, world);
+        success = success || spawn_spo_squad(event, player, world);
+    } else if (player.getFactionPoints(6) >= 4000 && player.getFactionPoints(6) < 5000) {
+        success = spawn_spo_fireteam(event, player, world);
+        success = success || spawn_spo_squad(event, player, world);
+        success = success || spawn_spo_squad(event, player, world);
+    } else {
+        success = spawn_spo_squad(event, player, world);
+        success = success || spawn_spo_squad(event, player, world);
+        success = success || spawn_spo_platoon(event, player, world);
+    }
+
+    if (success) {
+        player.message("&4You have been spotted and arrested by the police or SPO for your criminal activities! You shall perish in the name of the law!");
+    }
 }
 
-function setup_police_arrest(event, player, world) {
+// function to spawn a SPO Fireteam
+function spawn_spo_fireteam(event, player, world) {
+    // get a random number from 3 to 5
+    var count = Math.floor(Math.random() * 3) + 3;
+    // 3/4th of the team have shotguns
+    var shotgun = Math.floor(count * 0.75);
+    // 1/4th of the team have light assault rifles
+    var light_assault = Math.floor(count * 0.25);
+    // spawn the SPO dog
+    spawn_arrest(event, player, world, "dog_spo", 1, 10, 5);
+    // spawn the shotgun SPO
+    spawn_arrest(event, player, world, "shotgun_spo", shotgun, 10, 5);
+    // spawn the light assault SPO
+    spawn_arrest(event, player, world, "light_assault_spo", light_assault, 10, 5);
+}
 
-    var abandon = false;
+//function to spawn a SPO Squad
+function spawn_spo_squad(event, player, world) {
+    // get a random number from 8 to 12
+    var count = Math.floor(Math.random() * 5) + 8;
+    // 3/5th of the team have shotguns
+    var shotgun = Math.floor(count * 0.6);
+    // 2/5th of the team have light assault rifles
+    var light_assault = Math.floor(count * 0.4);
+    // spawn the SPO dog
+    spawn_arrest(event, player, world, "dog_spo", 1, 10, 5);
+    // spawn the shotgun SPO
+    spawn_arrest(event, player, world, "shotgun_spo", shotgun, 10, 5);
+    // spawn the light assault SPO
+    spawn_arrest(event, player, world, "light_assault_spo", light_assault, 10, 5);
+    // have 1/5 chance to spawn a sniper
+    if (Math.random() < 0.2) {
+        spawn_arrest(event, player, world, "sniper_spo", 1, 20, 5);
+    }
+}
+
+// function to spawn a SPO platoon
+function spawn_spo_platoon(event, player, world) {
+    // get a random number from 16 to 30
+    var count = Math.floor(Math.random() * 15) + 16;
+    // 3/6th of the team have shotguns
+    var shotgun = Math.floor(count * 0.5);
+    // 2/6th of the team have light assault rifles
+    var light_assault = Math.floor(count * 0.33);
+    // 1/6th of the team have sniper rifles
+    var sniper = Math.floor(count * 0.17);
+    // spawn the 2 to 4 SPO dogs
+    spawn_arrest(event, player, world, "dog_spo", Math.floor(Math.random() * 2) + 2, 10, 5);
+    // spawn the shotgun SPO
+    spawn_arrest(event, player, world, "shotgun_spo", shotgun, 10, 5);
+    // spawn the light assault SPO
+    spawn_arrest(event, player, world, "light_assault_spo", light_assault, 10, 5);
+    // spawn the sniper SPO
+    spawn_arrest(event, player, world, "sniper_spo", sniper, 20, 10);
+}
+
+function spawn_arrest(event, player, world, type, count, distance_from_player, group_radius) {
+
     var abandon_counter = 0;
     // get coordinates of the player
-    var x = player.getX();
+    var x = Math.floor(player.getX() + Math.random() * distance_from_player - (distance_from_player/2));
     var y = player.getY();
-    var z = player.getZ();
+    var z = Math.floor(player.getZ() + Math.random() * distance_from_player - (distance_from_player/2));
 
-    // get a random number between 1 and 4
-    var police_count = Math.floor(Math.random() * 4) + 1;
+    var success = false;
 
     // spawn the police
-    for (var i = 0; i < police_count; i++) {
+    for (var i = 0; i < count; i++) {
         // Chage the coordinates to a random value, in 5 blocks radius
-        x = x + Math.floor(Math.random() * 10) - 5;
-        y += 2;
-        z = z + Math.floor(Math.random() * 10) - 5;
+        x = x + Math.floor(Math.random() * group_radius) - (group_radius / 2);
+        y += 10;
+        z = z + Math.floor(Math.random() * group_radius) - (group_radius / 2);
 
         // floor the coordinates to the nearest block
         x = Math.floor(x);
@@ -65,18 +146,83 @@ function setup_police_arrest(event, player, world) {
 
 
         // fine an available y coordinate
-        while (world.getBlock(x, y, z).isAir() && abandon_counter < 10) {
+        while (world.getBlock(x, y, z).isAir() && abandon_counter < 30) {
             y--;
             abandon_counter++;
         }
         //player.message("Spawning the police at " + x + ", " + y + ", " + z);
 
         if (!world.getBlock(x, y, z).isAir()) {
-            world.spawnClone(x, y + 1, z, 9, "Police Arrest Clone");
-            player.message("You are a friend of the Criminals. You are under arrest!");
+            switch (type) {
+                case "police":
+                    spawn_police_NPC(x, y + 1, z, world);
+                    break;
+                case "dog_spo":
+                    spawn_spo_dog_NPC(x, y + 1, z, world);
+                    break;
+                case "shotgun_spo":
+                    spawn_shotgun_spo_NPC(x, y + 1, z, world);
+                    break;
+                case "light_assault_spo":
+                    spawn_light_assault_spo_NPC(x, y + 1, z, world);
+                    break;
+                case "sniper_spo":
+                    spawn_sniper_spo_NPC(x, y + 1, z, world);
+                    break;
+                default:
+                    spawn_police_NPC(x, y + 1, z, world);
+                    break;
+            }
+            success = true;
         }
-        /*else{
-            player.message("Could not find a suitable location to spawn the police. Abandoning the arrest.");
-        }*/
-    }   
+    }
+
+    return success;
+}
+
+// function to spawn a police officer
+function spawn_police_NPC(x, y, z, world) {
+    world.spawnClone(x, y, z, 9, "Police Arrest Clone");
+}
+
+// function to spawn a SPO dog
+function spawn_spo_dog_NPC(x, y, z, world) {
+    world.spawnClone(x, y, z, 9, "SPO dog Arrest Clone");
+}
+
+// function to spawn a shotgun SPO
+function spawn_shotgun_spo_NPC(x, y, z, world) {
+    world.spawnClone(x, y, z, 9, "SPO shotgun Arrest Clone");
+}
+
+//function to spawn a SPO light-assault
+function spawn_light_assault_spo_NPC(x, y, z, world) {
+    world.spawnClone(x, y, z, 9, "SPO light-assault Arrest Clone");
+}
+
+//function to spawn a SPO sniper
+function spawn_sniper_spo_NPC(x, y, z, world) {
+    world.spawnClone(x, y, z, 9, "SPO Sniper Arrest Clone");
+}
+
+// If player dies, remove the police
+function died(event) {
+    if (arrested_player_name == event.player.getDisplayName()) {
+        var player = event.player;
+        var world = player.world;
+        var nearby_gentity_list = world.getNearbyEntities(player.getPos(), 50, 0);
+        event.player.message("Nearby entities: " + nearby_gentity_list.length);
+        for (var i = 0; i < nearby_gentity_list.length; i++) {
+            var removal_test_entity = nearby_gentity_list[i];
+            event.player.message("Entity: " + removal_test_entity.getName());
+            if (removal_test_entity.getName().contains("Arrest Clone")) {
+                removal_test_entity.despawn();
+            }
+        }
+
+        //tell the player that his arrestation is done
+        player.message("&4Your arrest has been completed. You are now free to go.");
+        // remove 100 faction points from the player
+        player.addFactionPoints(6, -100);
+    }
 }
