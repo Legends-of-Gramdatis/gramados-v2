77268d59fbb87bd422d2b40693a1228d0df1681c
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2024-09-06 23:37:52 +0200

    full rework and refactor of aging shelf script. WIP

--- world/customnpcs/scripts/ecmascript/Gramados-Custom-NPCs-scripts/wine_aging_shelf.js
+++ world/customnpcs/scripts/ecmascript/Gramados-Custom-NPCs-scripts/wine_aging_shelf.js
@@ -1,186 +1,120 @@
 // Wine Aging Shelf
-// This script will add a aged timer to the item stack in the slot
-//
-// The GUI will have 6 slots for the player to place the wine bottles
+// This script adds a wine aging feature to the Growthcraft wine bottles.
+
+/*
+The block data must store an array of 32 IItemStacks.
+Each IItemStack must be some sort of wine bottle from the mod Growthcraft.
+Within each of those IItemStacks, there must be a custom NBT tag that will store the age of the wine bottle, and the date this bottle was placed in the shelf.
+On opening the GUI, the script will read the block data and generate a custom GUI with 32 bottle, that will be represented by a textured button per bottle. (32 buttons in total)
+Each bottle in stored data will also have its age increased by the time the bottle was in the shelf (aka add the time between now and the date the bottle was placed in the shelf to the age of the bottle).
+When the player clicks on a bottle, the button will be removed and the bottle will be removed from the block data.
+At the same time, the player will receive the bottle in his inventory as an Growthcraft wine bottle item with an extra NBT tag that will store the age of the wine bottle.
+The player can age further the bottle by placing it again in the shelf.
+*/
 
 
 // GUI
-var GUI, GRID_H = 16, GRID_W = 16, GRID_BORDER = 2;
-
+var GUI, GRID = 16;
 var owner = "";
+var BUTTON_TEXTURE_PATH = "minecraft:textures/gui/gramados_trucker_point_putton_textures.png";
 
-var stored_bottles = {
 
-    // slot 1
-    0: {
-        itemstack: "",
-        age: 0
-    },
-    // slot 2
-    1: {
-        itemstack: "",
-        age: 0
-    },
-    // slot 3
-    2: {
-        itemstack: "",
-        age: 0
-    },
-    // slot 4
-    3: {
-        itemstack: "",
-        age: 0
-    },
-    // slot 5
-    4: {
-        itemstack: "",
-        age: 0
-    },
-    // slot 6
-    5: {
-        itemstack: "",
-        age: 0
-    },
-    // slot 7
-    6: {
-        itemstack: "",
-        age: 0
-    },
-    // slot 8
-    7: {
-        itemstack: "",
-        age: 0
-    },
-    // slot 9
-    8: {
-        itemstack: "",
-        age: 0
-    }
-};
 
 function init(event) {
-    save_placeholder_item(event);
+    // If stored data is empty, create an empty array of 32 IItemStacks
+    if (event.block.storeddata.get("stored_bottles") == null) {
+        var stored_bottles = [];
+        for (var i = 0; i < 32; i++) {
+            stored_bottles.push(event.block.world.createItem("minecraft:air", 0, 1));
+        }
+        event.block.storeddata.put("stored_bottles", JSON.stringify(stored_bottles));
+    }
 }
 
 function interact(event) {
 
-    // get the item held by the player
+    var stored_bottles = JSON.parse(event.block.storeddata.get("stored_bottles"));
+
+    // tell the player the current data of the block
+    event.player.message("Stored items: " + JSON.stringify(stored_bottles));
+
+    // get what the player has in hand:
     var item = event.player.getMainhandItem();
-    if (item != null) {
-        // remove item by giving air
-        event.player.setMainhandItem(event.block.world.createItem("minecraft:air", 0, 1));
-        //event.player.message("Item held: " + item.getItemNbt().toJsonString());
-        event.player.updatePlayerInventory();
+    //get how many items the player has in hand
+    var item_count = item.getStackSize();
+
+    event.player.message("Item: " + item.getDisplayName() + " Count: " + item_count);
+
+    // If player holds air:
+    if (item.getDisplayName() == "Air") {
+        event.player.message("You are holding air.");
+
+        var bottle_count = 0;
+        // Count how many bottles are in the stored data
+        for (var i = 0; i < 32; i++) {
+            if (stored_bottles[i] != null) {
+                bottle_count++;
+            }
+        }
+
+        // Open the GUI
+        GUI = event.API.createCustomGui(1, GRID * 14, GRID * 11, false);
+        create_GUI(event, GRID, bottle_count, stored_bottles);
+        event.player.showCustomGui(GUI);
+
     } else {
-        event.player.message("No item held");
+
+        // get the first available slot of the array
+        for (var i = 0; i < 32; i++) {
+
+            var item_clone = item.copy();
+            item_clone.setStackSize(1);
+
+            // Add the age and date to the item
+            var item_nbt = item_clone.getItemNbt();
+            item_nbt.setLong("date", event.player.getWorld().getTotalTime());
+            item_nbt.setLong("age", 0);
+
+            if (stored_bottles[i] == null && item_count > 0) {
+
+
+                // add the item to the slot
+                stored_bottles[i] = item_nbt.toJsonString();
+                item_count--;
+
+                // tell the player that the item was placed in the shelf
+                event.player.message("You placed 1 item in the shelf: " + item_clone.getDisplayName() + " at slot " + i);
+            }
+        }
+
+        //tell what is in stored data
+        //event.player.message("Stored items: " + stored_bottles);
+
+        // Update stak size of the item in hand
+        item.setStackSize(item_count);
+        event.player.setMainhandItem(item);
+
+        // Save block data
+        event.block.storeddata.put("stored_bottles", JSON.stringify(stored_bottles));
+
     }
-
-    GUI = event.API.createCustomGui(1, GRID_W * 14, GRID_H * 11, false);
-
-    create_GUI(event, GRID_W, GRID_H);
-    GUI.showPlayerInventory(50, 100);
-
-    event.player.showCustomGui(GUI);
-
-    if (owner == "") {
-        owner = event.player.getName();
-    }
-
-    event.player.message("Owner: " + owner);
-
-    var custom_stored_bottles = JSON.parse(event.block.storeddata.get("stored_bottles"))
-    // if there is indeed a stored_bottles in the block data
-    if (custom_stored_bottles != null) {
-        custom_stored_bottles[0].age = 50
-        event.player.message(custom_stored_bottles[0].age)
-        event.player.message(stored_bottles[0].age)
-        //you'll notice that the age is different. custom_stored_bottles was recreated from the block's storeddata
-    }
-
-    readGuiSlots(event);
 
     return true;
 }
 
-function create_GUI(event, GRID_W, GRID_H) {
 
+
+
+
+
+function create_GUI(event, GRID, bottle_count, stored_bottles) {
+
+    event.player.message("Creating GUI with " + bottle_count + " bottles");
     GUI.setBackgroundTexture("minecraft:textures/gui/wine_aging_shelf_gui_wip.png");
     var button_texture_sheet = "minecraft:textures/gui/gramados_trucker_point_putton_textures.png";
 
-    // add 9 item slots
-    /*for (var i = 0; i < 9; i++) {
-
-        if (i == 0) {
-            var itemstack = load_placeholder_item(event);
-            GUI.addItemSlot(20 + (i * GRID_W), 20, itemstack);
-            event.player.message("Item added to slot 0");
-        } else {
-
-            GUI.addItemSlot(20 + (i * GRID_W), 20);
-            // add a red rectangle at the slot
-            GUI.addTexturedRect(i, button_texture_sheet, 20 + (i * GRID_W), 20, GRID_W, GRID_H, GRID_W * 8, 0);
-        }
-
-    }*/
-}
-
-function readGuiSlots(event) {
-    var slot_list = GUI.getSlots();
-    var slot_count = slot_list.length;
-    event.player.message("There are " + slot_count + " slots");
-    // navigate all slots
-    for (var i = 0; i < slot_count; i++) {
-
-        if (slot_list[i].hasStack()) {
-            event.player.message("Slot " + i + " has an item");
-        } else {
-            event.player.message("Slot " + i + " is empty");
-        }
+    // add the bottles as textures buttons
+    for (var i = 0; i < bottle_count; i++) {
+        GUI.addTexturedButton(i, "boottle", GRID * i, GRID, GRID, GRID, BUTTON_TEXTURE_PATH, GRID * 6, 0);
     }
-
-    // save data
-    //save_bottles_to_blockdata(event);
-}
-
-/*function customGuiSlot(event) {
-    event.player.message("Slot clicked");
-    readGuiSlots(event);
-}*/
-
-function save_bottles_to_blockdata(event) {
-    event.block.storeddata.put("stored_bottles", JSON.stringify(stored_bottles))
-    event.player.message("Bottles saved to block data");
-}
-
-function customGuiClose(event) {
-    event.player.message("GUI closed");
-    // save data
-    save_bottles_to_blockdata(event);
-}
-
-function fill_shelf_with_stored_bottles(event) {
-    var custom_stored_bottles = event.block.storeddata.get("stored_bottles")
-    event.player.message(custom_stored_bottles)
-    // if there is indeed a stored_bottles in the block data
-    if (custom_stored_bottles != null) {
-        for (var i = 0; i < 9; i++) {
-            if (!custom_stored_bottles[i].isEmpty()) {
-                var nbt_object = e.API.stringToNbt(nbtString)
-            }
-        }
-    }
-}
-
-function save_placeholder_item(event) {
-    var item = event.block.world.createItem("growthcraft_grapes:grapewine", 1, 4);
-    var itemAsNBT = item.getItemNbt().toJsonString()
-    event.block.storeddata.put("placeholderItem", itemAsNBT);
-}
-
-function load_placeholder_item(event) {
-    var nbtString = event.block.storeddata.get("placeholderItem");
-    event.player.message(nbtString);
-    var nbt_object = event.API.stringToNbt(nbtString);
-    var item = event.block.world.createItemFromNbt(nbt_object);
-    return item;
 }
