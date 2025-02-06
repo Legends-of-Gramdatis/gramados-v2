fb5b99e3dbc17ecac12c803118f308d343b9c666
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2025-02-06 01:35:31 +0100

    Update market configurations: standardize item IDs, adds support for multiple jobs of different types per region

--- world/customnpcs/scripts/ecmascript/Gramados-Custom-NPCs-scripts/job_manager.js
+++ world/customnpcs/scripts/ecmascript/Gramados-Custom-NPCs-scripts/job_manager.js
@@ -1,79 +1,28 @@
 var API = Java.type('noppes.npcs.api.NpcAPI').Instance();
-
-// Define the raw color codes and effects for formatting text
-var _RAWCOLORS = {
-    '0': 'black',
-    '1': 'dark_blue',
-    '2': 'dark_green',
-    '3': 'dark_aqua',
-    '4': 'dark_red',
-    '5': 'dark_purple',
-    '6': 'gold',
-    '7': 'gray',
-    '8': 'dark_gray',
-    '9': 'blue',
-    'a': 'green',
-    'b': 'aqua',
-    'c': 'red',
-    'd': 'light_purple',
-    'e': 'yellow',
-    'f': 'white',
-};
-var _RAWEFFECTS = {
-    'o': 'italic',
-    'l': 'bold',
-    'k': 'magic',
-    'm': 'strike',
-    'n': 'underline',
-    'r': 'reset'
-}
-var _ISLAND = [
-    "Gramados",
-    "Greenfield",
-    "Allenis"
-];
 var world = API.getIWorld(0);
 
 var data_file_path = "world/customnpcs/scripts/jobs_log.json";
 
-var Job_Active_Gramados_ID = 379;
-
 var job_data;
-
 var tick_counter_max = 1;
 var tick_counter = 1;
 
-// Load the stock exchange data when the NPC is initialized
+// Load Job Data on NPC Initialization
 function init(event) {
-    // world.broadcast("Job Manager initialized for player " + event.player.getName());
-
-    // Load the Job data
     job_data = load_json(data_file_path);
-
-    // If the Job data doesn't exist, create it
     if (!job_data) {
-        job_data = {
-            "Region_To_ID": {},
-            "Regions_To_Quit_ID": {},
-            "Starter_Jobs": []
-        };
+        job_data = {"Region_Job_Limits": {}, "Jobs": [] };
         save_json(job_data, data_file_path);
     }
-
+    check_all_jobs(event.player);
     update_job_entries(event.player);
-
     tell_player_job(event.player);
-
     update_job_perms(event.player);
-
-    // world.broadcast("Job Data: " + JSON.stringify(job_data));
+    update_job_type_json(event.player);
 }
 
-//Tick
+// Tick Function
 function tick(event) {
-    //world.broadcast("Job Manager ticked!");
-
-    // Check if the Job data is loaded
     if (!job_data) {
         world.broadcast("ERROR: Job data not loaded!");
         return;
@@ -81,10 +30,7 @@ function tick(event) {
 
     if (tick_counter >= tick_counter_max) {
         tick_counter = 0;
-
-        // Check all jobs
         check_all_jobs(event.player);
-        // Quit a job
         quit_job_manager(event.player);
     } else {
         tick_counter++;
@@ -106,149 +52,7 @@ function update_job_entries(player) {
 
         save_json(job_data, data_file_path);
 
-        world.broadcast("New Job entry created for player " + player.getName());
-        // world.broadcast("Job Data: " + JSON.stringify(job_data));
-    }
-}
-
-//function to check all jobs
-function check_all_jobs(player) {
-    // Get the UUID of the player
-    var player_uuid = player.getUUID();
-
-    // world.broadcast("Checking all jobs for player " + player.getName());
-
-    for (var i = 0; i < job_data["Starter_Jobs"].length; i++) {
-
-        // world.broadcast("Checking job " + job_data["Starter_Jobs"][i]["JobName"]);
-
-        var job_id = job_data["Starter_Jobs"][i]["JobID"];
-        var job_name = job_data["Starter_Jobs"][i]["JobName"];
-        var job_region = job_data["Starter_Jobs"][i]["Region"];
-        var job_region_id = job_data["Region_To_ID"][job_region];
-
-        // Check if the player already had the dialogue of ID job_id
-        if (player.hasReadDialog(job_id) && !player.hasReadDialog(job_region_id)) {
-
-            // if this is not already in the player's Active Jobs
-            if (!job_data[player_uuid]["ActiveJobs"][job_id]) {
-
-                // Add the job to the player's Active Jobs
-                job_data[player_uuid]["ActiveJobs"][job_id] = {
-                    "JobID": job_id,
-                    "JobName": job_name,
-                    "StartTime": world.getTotalTime(),
-                    "Region": job_region
-                };
-
-                // Make it so the player has read the region dialogue, preventing any more jobs from this region to be added
-                // Until he quits the job
-                player.addDialog(job_region_id);
-
-                world.broadcast(player.getName() + " has started job \"" + job_name + "\" with ID " + job_id);
-
-                update_job_perms(player);
-                // world.broadcast("Job Data: " + JSON.stringify(job_data));
-                save_json(job_data, data_file_path);
-            }
-        }
-
-    }
-}
-
-// function to manage a job quit
-function quit_job_manager(player) {
-    // For all regions in "Regions_To_Quit_ID"
-    for (var region in job_data["Regions_To_Quit_ID"]) {
-        var region_quit_id = job_data["Regions_To_Quit_ID"][region];
-        var region_id = job_data["Region_To_ID"][region];
-
-        // If the player is trying to quit a job
-        if (player.hasReadDialog(region_quit_id)) {
-            // For all active jobs
-            for (var job in job_data[player.getUUID()]["ActiveJobs"]) {
-                // If the job is in the region
-                if (job_data[player.getUUID()]["ActiveJobs"][job]["Region"] == region) {
-                    // If it's been more than 24 hours since the job started
-                    if (world.getTotalTime() - job_data[player.getUUID()]["ActiveJobs"][job]["StartTime"] >= 1728000) {
-
-                        // Creat the data for his job history
-                        var job_history = {
-                            "JobID": job_data[player.getUUID()]["ActiveJobs"][job]["JobID"],
-                            "JobName": job_data[player.getUUID()]["ActiveJobs"][job]["JobName"],
-                            "StartTime": job_data[player.getUUID()]["ActiveJobs"][job]["StartTime"],
-                            "EndTime": world.getTotalTime(),
-                            "Region": job_data[player.getUUID()]["ActiveJobs"][job]["Region"]
-                        };
-
-                        // Add the job to the player's Job History
-                        job_data[player.getUUID()]["JobHistory"][job] = job_history;
-
-                        // Remove the job from the player's Active Jobs
-                        player.removeDialog(job_data[player.getUUID()]["ActiveJobs"][job]["JobID"]);
-                        delete job_data[player.getUUID()]["ActiveJobs"][job];
-                        player.removeDialog(region_id);
-                        player.removeDialog(region_quit_id);
-
-                        world.broadcast(player.getName() + " has quit his job in region " + region);
-                        // world.broadcast("Job Data: " + JSON.stringify(job_data));
-                        save_json(job_data, data_file_path);
-
-                        // remove jobs perms
-                        remove_job_perms(player, job_history["JobID"]);
-                    } else {
-                        player.message("You can't quit a job in less than 24 hours!");
-                        player.removeDialog(region_quit_id);
-                    }
-                }
-            }
-        }
-    }
-}
-
-// function to load Job data
-function load_json(data_file_path) {
-    // Check if the file exists, and create it if it doesn't
-    if (!check_file_exists(data_file_path)) {
-        world.broadcast("ERROR: Job Data is inexistant!");
-        return null;
-    } else {
-        var ips = new java.io.FileInputStream(data_file_path);
-        var fileReader = new java.io.InputStreamReader(ips, "UTF-8");
-        var readFile = fileReader.read();
-        var data;
-        var start = "";
-        while (readFile != -1) {
-            data = String.fromCharCode(readFile);
-            start = start + data;
-            readFile = fileReader.read();
-        }
-
-        var json_data = JSON.parse(start);
-
-        // world.broadcast("Loaded data: " + JSON.stringify(json_data));
-
-        return json_data;
-    }
-}
-
-// Function to save Job data
-function save_json(data, data_file_path) {
-    var fileWriter = new java.io.FileWriter(data_file_path);
-    fileWriter.write(JSON.stringify(data, null, 4)); // Pretty-print JSON with 4 spaces
-    fileWriter.close();
-}
-
-// function to check if a file exists
-function check_file_exists(file_path) {
-    var file = new java.io.File(file_path);
-    return file.exists();
-}
-
-// Function to tell a message to teh player only if teh player is called TheOddlySeagull
-function tell_player(player, message) {
-    if (player.getName() == "TheOddlySeagull") {
-        player.message(message);
+        player.message("Welcome to the Gramados Server! You have been registered as a new player! Get a job to start earning money!");
     }
 }
 
@@ -284,6 +88,18 @@ function get_time(start_time_ticks, end_time_ticks) {
     return years + " years, " + months + " months, " + days + " days, " + hours + " hours, " + minutes + " minutes and " + seconds + " seconds";
 }
 
+// Count Active Jobs by Type in a Region
+function count_jobs_by_type_in_region(player_uuid, region, job_type) {
+    var count = 0;
+    for (var job in job_data[player_uuid]["ActiveJobs"]) {
+        if (job_data[player_uuid]["ActiveJobs"][job]["Region"] === region &&
+            job_data[player_uuid]["ActiveJobs"][job]["Type"] === job_type) {
+            count++;
+        }
+    }
+    return count;
+}
+
 //function to update job perms
 function update_job_perms(player) {
     var player_uuid = player.getUUID();
@@ -299,12 +115,12 @@ function add_job_perms(player, job_id) {
     // Load world data
     var world_data = world.getStoreddata();
 
-    for (var i = 0; i < job_data["Starter_Jobs"].length; i++) {
-        if (job_data["Starter_Jobs"][i]["JobID"] == job_id) {
-            if (job_data["Starter_Jobs"][i]["Perms"]) {
-                // world.broadcast("Adding perms for job " + job_data["Starter_Jobs"][i]["JobName"] + " to player " + player.getName());
-                for (var j = 0; j < job_data["Starter_Jobs"][i]["Perms"].length; j++) {
-                    var perm = job_data["Starter_Jobs"][i]["Perms"][j];
+    for (var i = 0; i < job_data["Jobs"].length; i++) {
+        if (job_data["Jobs"][i]["JobID"] == job_id) {
+            if (job_data["Jobs"][i]["Perms"]) {
+                // world.broadcast("Adding perms for job " + job_data["Jobs"][i]["JobName"] + " to player " + player.getName());
+                for (var j = 0; j < job_data["Jobs"][i]["Perms"].length; j++) {
+                    var perm = job_data["Jobs"][i]["Perms"][j];
 
                     // world.broadcast("Adding perm " + perm + " to player " + player.getName());
                     // world.broadcast("Old perm data: " + JSON.stringify(world_data.get(perm)));
@@ -347,11 +163,11 @@ function remove_job_perms(player, job_id) {
     // Load world data
     var world_data = world.getStoreddata();
 
-    for (var i = 0; i < job_data["Starter_Jobs"].length; i++) {
-        if (job_data["Starter_Jobs"][i]["JobID"] == job_id) {
-            if (job_data["Starter_Jobs"][i]["Perms"]) {
-                for (var j = 0; j < job_data["Starter_Jobs"][i]["Perms"].length; j++) {
-                    var perm = job_data["Starter_Jobs"][i]["Perms"][j];
+    for (var i = 0; i < job_data["Jobs"].length; i++) {
+        if (job_data["Jobs"][i]["JobID"] == job_id) {
+            if (job_data["Jobs"][i]["Perms"]) {
+                for (var j = 0; j < job_data["Jobs"][i]["Perms"].length; j++) {
+                    var perm = job_data["Jobs"][i]["Perms"][j];
 
                     /* 
                     The line looks like something like this:
@@ -374,5 +190,183 @@ function remove_job_perms(player, job_id) {
                 }
             }
         }
+    }
+}
+
+
+// Check All Jobs
+function check_all_jobs(player) {
+    var player_uuid = player.getUUID();
+
+    for (var i = 0; i < job_data["Jobs"].length; i++) {
+
+        // Check if the job is already active
+        if (!job_data[player_uuid]["ActiveJobs"][job_data["Jobs"][i]["JobID"]]) {
+
+            var job = job_data["Jobs"][i];
+            var job_id = job["JobID"];
+            var job_name = job["JobName"];
+            var job_region = job["Region"];
+            var job_type = job["Type"];
+
+            var job_limit = job_data["Region_Job_Limits"][job_region] ? job_data["Region_Job_Limits"][job_region][job_type] : 1;
+
+            if (player.hasReadDialog(job_id)) {
+                if (!job_data[player_uuid]["ActiveJobs"][job_id] &&
+                    count_jobs_by_type_in_region(player_uuid, job_region, job_type) < job_limit) {
+
+                    job_data[player_uuid]["ActiveJobs"][job_id] = {
+                        "JobID": job_id,
+                        "JobName": job_name,
+                        "StartTime": world.getTotalTime(),
+                        "Region": job_region,
+                        "Type": job_type
+                    };
+
+                    world.broadcast(player.getName() + " has started job \"" + job_name + "\" in " + job_region);
+                    update_job_perms(player);
+                    save_json(job_data, data_file_path);
+                } else {
+                    player.message("You have reached the limit for " + job_type + " jobs in " + job_region + "! You cannot join " + job_name + " as you already have " + get_jobs_by_type_in_region(player_uuid, job_region, job_type));
+                    player.removeDialog(job_id);
+                }
+            }
+        }
+    }
+}
+
+// Quit Job Manager
+function quit_job_manager(player) {
+    var player_uuid = player.getUUID();
+
+    for (var i = 0; i < job_data["Jobs"].length; i++) {
+        var job = job_data["Jobs"][i];
+
+        if (job["JobQuit"] && player.hasReadDialog(job["JobQuit"])) {
+            var job_id = job["JobID"];
+
+            // Check if the player actually has this job
+            if (!job_data[player_uuid]["ActiveJobs"][job_id]) {
+                player.message("ERROR: You cannot quit a job you don't have!");
+                player.removeDialog(job["JobQuit"]); // Remove the quit dialogue
+                continue;
+            }
+
+            var active_job = job_data[player_uuid]["ActiveJobs"][job_id];
+
+            // Ensure player has worked the job for at least 24 in-game hours (20 ticks per second)
+            var min_work_time = 20 * 60 * 60 * 24; // 24 real-time hours in ticks
+            if (world.getTotalTime() - active_job["StartTime"] < min_work_time) {
+                player.message("You need to work at least 24 hours before quitting this job!");
+                player.removeDialog(job["JobQuit"]); // Remove the quit dialogue
+                continue;
+            }
+
+            // Move job to history
+            var job_history = {
+                "JobID": active_job["JobID"],
+                "JobName": active_job["JobName"],
+                "StartTime": active_job["StartTime"],
+                "EndTime": world.getTotalTime(),
+                "Region": active_job["Region"]
+            };
+            job_data[player_uuid]["JobHistory"][job_id] = job_history;
+
+            // Remove job from active jobs
+            delete job_data[player_uuid]["ActiveJobs"][job_id];
+
+            // Remove job-related dialogs
+            player.removeDialog(job["JobQuit"]);
+            player.removeDialog(job["JobID"]);
+
+            // Remove permissions
+            remove_job_perms(player, job_id);
+
+            // Broadcast quit message
+            world.broadcast(player.getName() + " has quit their job as " + job["JobName"] + " in " + job["Region"]);
+
+            // Save changes
+            save_json(job_data, data_file_path);
+        }
+    }
+}
+
+// Load JSON
+function load_json(data_file_path) {
+    if (!check_file_exists(data_file_path)) {
+        world.broadcast("ERROR: Job Data is nonexistent!");
+        return null;
+    } else {
+        var ips = new java.io.FileInputStream(data_file_path);
+        var fileReader = new java.io.InputStreamReader(ips, "UTF-8");
+        var start = "";
+        var readFile = fileReader.read();
+        while (readFile != -1) {
+            start += String.fromCharCode(readFile);
+            readFile = fileReader.read();
+        }
+        return JSON.parse(start);
+    }
+}
+
+// Save JSON
+function save_json(data, data_file_path) {
+    var fileWriter = new java.io.FileWriter(data_file_path);
+    fileWriter.write(JSON.stringify(data, null, 4));
+    fileWriter.close();
+}
+
+// Check if File Exists
+function check_file_exists(file_path) {
+    return new java.io.File(file_path).exists();
+}
+
+// Returns the list of jobs you currently have in a given region with the given type
+function get_jobs_by_type_in_region(player_uuid, region, job_type) {
+    var jobs = [];
+    for (var job in job_data[player_uuid]["ActiveJobs"]) {
+        if (job_data[player_uuid]["ActiveJobs"][job]["Region"] === region &&
+            job_data[player_uuid]["ActiveJobs"][job]["Type"] === job_type) {
+            jobs.push(job_data[player_uuid]["ActiveJobs"][job]["JobName"]);
+        }
+    }
+    // make a string out of the array
+    jobs = jobs.join(", ");
+    return jobs;
+}
+
+// Function to update the player's job type json if necessary as this is a new feature and not all players have it
+function update_job_type_json(player) {
+    var player_uuid = player.getUUID();
+
+    var updated = false;
+
+    // Check all active and history jobs
+    for (var job in job_data[player_uuid]["ActiveJobs"]) {
+        if (!job_data[player_uuid]["ActiveJobs"][job]["Type"]) {
+            for (var i = 0; i < job_data["Jobs"].length; i++) {
+                if (job_data["Jobs"][i]["JobID"] == job) {
+                    job_data[player_uuid]["ActiveJobs"][job]["Type"] = job_data["Jobs"][i]["Type"];
+                    world.broadcast("Updated job type for player " + player.getName() + " to " + job_data["Jobs"][i]["Type"]);
+                    updated = true;
+                }
+            }
+        }
+    }
+
+    for (var job in job_data[player_uuid]["JobHistory"]) {
+        if (!job_data[player_uuid]["JobHistory"][job]["Type"]) {
+            for (var i = 0; i < job_data["Jobs"].length; i++) {
+                if (job_data["Jobs"][i]["JobID"] == job) {
+                    job_data[player_uuid]["JobHistory"][job]["Type"] = job_data["Jobs"][i]["Type"];
+                    world.broadcast("Updated job type for player " + player.getName() + " to " + job_data["Jobs"][i]["Type"]);
+                    updated = true;
+                }
+            }
+        }
+    }
+
+    if (updated) {
+        save_json(job_data, data_file_path);
     }
 }
