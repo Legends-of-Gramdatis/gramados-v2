03013087b11ddd60760de3694be86c02197c1675
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2025-02-25 14:25:36 +0100

    Upgrade & Event System Foundation #36 - Refactor shopkeeping upgrade and event functions: consolidate loading logic, enhance event handling, and improve output formatting

--- world/customnpcs/scripts/ecmascript/modules/shopkeeping_dev/shopkeeping_upgrades.js
+++ world/customnpcs/scripts/ecmascript/modules/shopkeeping_dev/shopkeeping_upgrades.js
@@ -1,46 +1,20 @@
 var UPGRADES_JSON_PATH = "world/customnpcs/scripts/ecmascript/modules/shopkeeping_dev/shop_upgrades.json";
 
 // function that gets the list of upgrades from json
-function loadUpgrades(player) {
-    var result = {
-        "upgrades" : [],
-        "events" : []
-    }
-
+function loadUpgradesAndEvents(player) {
     var jsonUpgrades = loadJson(UPGRADES_JSON_PATH);
     if (!jsonUpgrades) {
         tellPlayer(player, "&4No upgrades found! Contact an admin!");
     }
-    // tellPlayer(player, "&6Upgrades found: " + jsonUpgrades.upgrades.length);
 
-    for (var i = 0; i < jsonUpgrades.upgrades.length; i++) {
-        var upgrade = jsonUpgrades.upgrades[i];
-        // tellPlayer(player, "&6Checking upgrade " + upgrade["name"]);
-        if (isUpgradeRepeatable(player, upgrade)) {
-            result.events.push(upgrade);
-            // tellPlayer(player, "&6Upgrade " + upgrade.name + " is repeatable!");
-        } else {
-            result.upgrades.push(upgrade);
-            // tellPlayer(player, "&6Upgrade " + upgrade.name + " is not repeatable!");
-        }
-    }
-
-    return result;
+    return jsonUpgrades;
 }
 
-// function to get if an upgrade is repeatable or not
-function isUpgradeRepeatable(player, upgrade) {
-    // tellPlayer(player, "Checking upgrade " + upgrade.name);
-    // If it has an "repeatable" key, return true
-    return upgrade.repeatable;
-}
-
-// function to display a formatted list of upgrades and events
-function listUpgrades(player) {
-    var upgrades = loadUpgrades(player);
+// function to display a formatted list of upgrades
+function listAllUpgrades(player) {
+    var upgrades = loadUpgradesAndEvents(player);
 
     var messageUpgrades = [];
-    var messageEvents = [];
 
     for (var i = 0; i < upgrades.upgrades.length; i++) {
         var upgrade = upgrades.upgrades[i];
@@ -50,48 +24,69 @@ function listUpgrades(player) {
         messageUpgrades.push(entry);
     }
 
-    for (var i = 0; i < upgrades.events.length; i++) {
-        var event = upgrades.events[i].name;
-        messageEvents.push(event);
-    }
-
     tellPlayer(player, "&bUpgrades: ");
     storytellPlayer(player, messageUpgrades);
-    tellPlayer(player, "&bEvents: ");
+}
+
+// function to display a formatted list of events
+function listAllEvents(player) {
+    var upgrades = loadUpgradesAndEvents(player);
+
+    var messageEvents = [];
+
+    for (var i = 0; i < upgrades.events.length; i++) {
+        var event = upgrades.events[i];
+        var entry = "&6" + event.name + "&f: \n &7" + event.description + "\n &6Cost: &r:money:&e" + getAmountCoin(event.cost);
+        messageEvents.push(entry);
+    }
+    tellPlayer(player, "&b=========================================\n&bUpgrades: \n&b=========================================");
     storytellPlayer(player, messageEvents);
 }
 
-// function to display a formatted list of upgrades and events for a shop
+// function to display a formatted list of upgrades with availability for a shop
 function listShopUpgrades(player, shopId) {
-    var upgrades = loadUpgrades(player);
-    var shopUpgrades = getShopUpgrades(player, shopId);
+    var upgrades = loadUpgradesAndEvents(player);
 
     var messageUpgrades = [];
     var messageEvents = [];
 
     for (var i = 0; i < upgrades.upgrades.length; i++) {
-        tellPlayer(player, "&6Checking if shop can take upgrade " + upgrades.upgrades[i].name);
-        // Check if the shop has the upgrades if this upgrade is dependent on another
         var upgrade = upgrades.upgrades[i];
         var canTakeUpgrade = canShopTakeUpgrade(player, shopId, upgrade);
 
         if (canTakeUpgrade.canTake) {
-            var entry = "&6" + upgrade.name + "&f: \n &7" + upgrade.description + "\n &6Cost: &r:money:&e" + getAmountCoin(upgrade.cost);
+            var entry = "&2:check: &a" + upgrade.name + ": \n&7" + upgrade.description + "\n&6Cost: &r:money:&e" + getAmountCoin(upgrade.cost) + "\n&a:thumbsup: Shop can take this upgrade!\n&b-----------------------------------------";
             messageUpgrades.push(entry);
         } else {
-            var entry = "&6" + upgrade.name + "&f: \n &7" + upgrade.description + "\n &6Cost: &r:money:&e" + getAmountCoin(upgrade.cost) + "\n &4" + canTakeUpgrade.messages.join("\n &4");
+            var entry = "&4:cross: &c" + upgrade.name + ":" + "\n &c- " + canTakeUpgrade.messages.join("\n &c- ") + "\n &7" + upgrade.description + "\n&6Cost: &r:money:&e" + getAmountCoin(upgrade.cost) + "\n&c:thumbsdown: Shop can't take this upgrade! \n&b-----------------------------------------";
             messageUpgrades.push(entry);
         }
     }
 
+    tellPlayer(player, "&b=========================================\n&bUpgrades: \n&b=========================================");
+    storytellPlayer(player, messageUpgrades);
+}
+
+// function to display a formatted list of events with availability for a shop
+function listShopEvents(player, shopId) {
+    var upgrades = loadUpgradesAndEvents(player);
+
+    var messageEvents = [];
+
     for (var i = 0; i < upgrades.events.length; i++) {
-        var event = upgrades.events[i].name;
-        messageEvents.push(event);
+        var event = upgrades.events[i];
+        var canTakeUpgrade = canShopTakeUpgrade(player, shopId, event);
+
+        if (canTakeUpgrade.canTake) {
+            var entry = "&2:check: &a" + event.name + ": \n&7" + event.description + "\n&a:thumbsup: Shop can start this event!\n&b-----------------------------------------";
+            messageEvents.push(entry);
+        } else {
+            var entry = "&4:cross: &c" + event.name + ":" + "\n &c- " + canTakeUpgrade.messages.join("\n &c- ") + "\n &7" + event.description + "\n&c:thumbsdown: Shop can't start this event! \n&b-----------------------------------------";
+            messageEvents.push(entry);
+        }
     }
 
-    tellPlayer(player, "&bUpgrades: ");
-    storytellPlayer(player, messageUpgrades);
-    tellPlayer(player, "&bEvents: ");
+    tellPlayer(player, "&b=========================================\n&bEvents: \n&b=========================================");
     storytellPlayer(player, messageEvents);
 }
 
@@ -103,6 +98,7 @@ function canShopTakeUpgrade(player, shopId, upgrade) {
         "canTake" : true,
         "messages" : []
     }
+
     var playerShops = loadJson(SERVER_SHOPS_JSON_PATH);
     var shopData = playerShops[shopId];
     if (!shopData) {
@@ -121,12 +117,10 @@ function canShopTakeUpgrade(player, shopId, upgrade) {
         if (shopData.reputation_data.reputation < requiredReputation) {
             result.canTake = false;
             result.messages.push("Shop doesn't have the required reputation!");
-            return result;
         }
         if (upgrade.max_reputation && shopData.reputation_data.reputation > upgrade.max_reputation) {
             result.canTake = false;
             result.messages.push("Shop has too much reputation!");
-            return result;
         }
     }
 
@@ -134,24 +128,61 @@ function canShopTakeUpgrade(player, shopId, upgrade) {
     if (!hasDependentUpgrades(player, upgrade, shopData.upgrades)) {
         result.canTake = false;
         result.messages.push("Shop doesn't have the required upgrades!");
-        return result;
     }
 
-    // if ungrade cost is inferior to the shop's money, return true
-    if (upgrade.cost <= shopData.finances.stored_cash) {
-        return result;
+    // if upgrade cost is superior to the shop's money
+    if (upgrade.cost > shopData.finances.stored_cash) {
+        result.canTake = false;
+        result.messages.push("Shop doesn't have enough money!");
     }
+
+    return result;
 }
 
-// function to get shop's current upgrades (list of ids)
-function getShopUpgrades(player, shopId) {
+function canShopStartEvent(player, shopId, event) {
+    var result = {
+        "canTake" : true,
+        "messages" : []
+    }
+
     var playerShops = loadJson(SERVER_SHOPS_JSON_PATH);
     var shopData = playerShops[shopId];
-    // tellPlayer(player, "Shop data: " + JSON.stringify(shopData));
+    if (!shopData) {
+        result.canTake = false;
+        result.messages.push("Shop data not found!");
+        return result;
+    }
 
-    // tellPlayer(player, "Shop data: " + shopData.upgrades);
+    if (!shopData.events) {
+        shopData.events = [];
+    }
 
-    return shopData.upgrades;
+    // if event has "min_reputation" key, check if the shop has the required reputation
+    if (event.min_reputation) {
+        var requiredReputation = event.min_reputation;
+        if (shopData.reputation_data.reputation < requiredReputation) {
+            result.canTake = false;
+            result.messages.push("Shop doesn't have the required reputation!");
+        }
+        if (event.max_reputation && shopData.reputation_data.reputation > event.max_reputation) {
+            result.canTake = false;
+            result.messages.push("Shop has too much reputation!");
+        }
+    }
+
+    // if event has "requires" key, check if the shop has the required upgrades
+    if (!hasDependentUpgrades(player, event, shopData.upgrades)) {
+        result.canTake = false;
+        result.messages.push("Shop doesn't have the required upgrades!");
+    }
+
+    // if event cost is superior to the shop's money
+    if (event.cost > shopData.finances.stored_cash) {
+        result.canTake = false;
+        result.messages.push("Shop doesn't have enough money!");
+    }
+
+    return result;
 }
 
 // Function to see if one upgarde has dependent upgrades and if they are satisfied
@@ -165,12 +196,12 @@ function hasDependentUpgrades(player, upgrade, availableUpgrades) {
     for (var i = 0; i < dependency_count; i++) {
         var dependency = dependencies[i];
         if (!includes(availableUpgrades, dependency)) {
-            tellPlayer(player, "&cShop doesn't have the required upgrades!");
+            // tellPlayer(player, "&cShop doesn't have the required upgrades!");
             return false;
         }
     }
 
-    tellPlayer(player, "&aShop has the required upgrades!");
+    // tellPlayer(player, "&aShop has the required upgrades!");
 
     return true;
 }
