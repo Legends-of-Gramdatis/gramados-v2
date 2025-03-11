777faa9cbed487b0f0cd02940b7cc9dfa5b6c19b
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2025-03-11 09:18:20 +0100

    Add romanize function to convert numbers to roman numerals

--- world/customnpcs/scripts/ecmascript/gramados_utils/utils_chat.js
+++ world/customnpcs/scripts/ecmascript/gramados_utils/utils_chat.js
@@ -423,3 +423,21 @@ function executeCommandGlobal(command, dim) {
     if (typeof (dim) == typeof (undefined) || dim === null) { dim = 0; }
     return API.createNPC(API.getIWorld(dim).getMCWorld()).executeCommand(command);
 }
+
+/**
+ * Converts a number to a roman numeral.
+ * @param {number} num - The number to convert.
+ * @returns {string} The roman numeral.
+ */
+function romanize(num) {
+    var lookup = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 },
+        roman = '',
+        i;
+    for (i in lookup) {
+        while (num >= lookup[i]) {
+            roman += i;
+            num -= lookup[i];
+        }
+    }
+    return roman;
+}
