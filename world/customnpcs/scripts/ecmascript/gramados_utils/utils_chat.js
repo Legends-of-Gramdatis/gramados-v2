83ec4a4a3fb6999bde52d31bdb048c39c61d2c13
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2025-03-11 08:49:09 +0100

    Fix slugify function to correctly handle underscores in text

--- world/customnpcs/scripts/ecmascript/gramados_utils/utils_chat.js
+++ world/customnpcs/scripts/ecmascript/gramados_utils/utils_chat.js
@@ -115,11 +115,11 @@ function stripColors(str) {
 function slugify(text) {
     text = stripColors(text);
     return text.toString().toLowerCase()
-        .replace(/\s+/g, '_') // Replace spaces with -
+        .replace(/\s+/g, '_') // Replace spaces with _
         .replace(/[^\w\-]+/g, '') // Remove all non-word chars
-        .replace(/__+/g, '_') // Replace multiple - with single -
-        .replace(/^_+/, '') // Trim - from start of text
-        .replace(/_+$/, ''); // Trim - from end of text
+        .replace(/__+/g, '_') // Replace multiple _ with single _
+        .replace(/^_+/, '') // Trim _ from start of text
+        .replace(/_+$/, ''); // Trim _ from end of text
 }
 
 /**
