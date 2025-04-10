0b01528a90b0bd89f74a5ed40819e6bd4b31a0f2
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2025-04-10 22:11:51 +0200

    Add isDateBetween and isDateBetweenYear functions to check date ranges

--- world/customnpcs/scripts/ecmascript/gramados_utils/utils_date.js
+++ world/customnpcs/scripts/ecmascript/gramados_utils/utils_date.js
@@ -13,3 +13,20 @@ function isDateBetween(startDay, startMonth, endDay, endMonth) {
 
     return currentDate >= startDate && currentDate <= endDate;
 }
+
+/**
+ * Checks if the current date is between two specified dates in a specific year.
+ * @param {number} startDay - The start day of the range.
+ * @param {number} startMonth - The start month of the range.
+ * @param {number} endDay - The end day of the range.
+ * @param {number} endMonth - The end month of the range.
+ * @param {number} year - The year to check against.
+ * @returns {boolean} - True if the current date is between the specified dates, false otherwise.
+ */
+function isDateBetweenYear(startDay, startMonth, endDay, endMonth, year) {
+    var currentDate = new Date(year, startMonth, startDay);
+    var startDate = new Date(year, startMonth, startDay);
+    var endDate = new Date(year, endMonth, endDay);
+
+    return currentDate >= startDate && currentDate <= endDate;
+}
