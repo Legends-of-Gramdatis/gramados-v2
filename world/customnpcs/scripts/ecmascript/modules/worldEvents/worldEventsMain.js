93c120b69baf890d360d002e4e1dfc4139488378
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2025-04-10 23:22:08 +0200

    Refactor world events script: remove unused player event data functions and streamline event initialization

--- world/customnpcs/scripts/ecmascript/modules/worldEvents/worldEventsMain.js
+++ world/customnpcs/scripts/ecmascript/modules/worldEvents/worldEventsMain.js
@@ -7,43 +7,19 @@ load('world/customnpcs/scripts/ecmascript/gramados_utils/utils_files.js');
 load("world/customnpcs/scripts/ecmascript/gramados_utils/utils_maths.js");
 load("world/customnpcs/scripts/ecmascript/gramados_utils/utils_chat.js");
 load("world/customnpcs/scripts/ecmascript/gramados_utils/utils_logging.js");
+load("world/customnpcs/scripts/ecmascript/gramados_utils/utils_date.js");
 
-load("world/customnpcs/scripts/ecmascript/modules/worldEvents/events/susBoxEvent.js")
+load("world/customnpcs/scripts/ecmascript/modules/worldEvents/events/susBoxEvent.js");
+load("world/customnpcs/scripts/ecmascript/modules/worldEvents/events/easter/easterEggHuntEvent.js");
 
 var API = Java.type('noppes.npcs.api.NpcAPI').Instance()
 var counter = 0;
-
-var PLAYER_EVENT_DATA = "world/customnpcs/scripts/ecmascript/modules/worldEvents/player_event_data.json";
 
 var playerLastSpawnTime = {}; // Tracks the last spawn time for each player in milliseconds
 var playerSpawnIntervals = {}; // Tracks the spawn interval for each player in milliseconds
 
 var EVENT_CONFIG_FILE_PATH = "world/customnpcs/scripts/ecmascript/modules/worldEvents/event_config.json";
 var allEventConfig = loadEventConfig();
-
-/**
- * Saves the playerLastSpawnTime and playerSpawnIntervals data to a JSON file.
- */
-function savePlayerSpawnData() {
-    saveJson({ lastSpawnTime: playerLastSpawnTime, spawnIntervals: playerSpawnIntervals }, PLAYER_EVENT_DATA);
-}
-
-/**
- * Loads the player event data from a JSON file.
- */
-function loadPlayerEventData(event_name, player_name) {
-    if (checkFileExists(PLAYER_EVENT_DATA)) {
-        var all_data = loadJson(PLAYER_EVENT_DATA);
-
-        if (all_data[event_name] && all_data[event_name][player_name]) {
-            return all_data[event_name][player_name];
-        } else {
-            return {};
-        }
-    } else {
-        return {};
-    }
-}
 
 /**
  * Loads the event configuration from the JSON file.
@@ -54,28 +30,6 @@ function loadEventConfig() {
         throw new Error("Event configuration file not found: " + EVENT_CONFIG_FILE_PATH);
     }
     return loadJson(EVENT_CONFIG_FILE_PATH);
-}
-
-/**
- * Checks if the current date falls within the specified event's date range.
- * @param {string} eventName - The name of the event to check.
- * @returns {boolean} - True if the current date is within the event range, false otherwise.
- */
-function isEventActive(eventName) {
-    var currentDate = new Date();
-    for (var i = 0; i < allEventConfig.events.length; i++) {
-        var eventConfig = allEventConfig.events[i];
-        if (
-            eventConfig.name === eventName &&
-            ((currentDate.getDate() >= eventConfig.startDate.day
-            && currentDate.getMonth() == eventConfig.startDate.month)
-            || (currentDate.getDate() <= eventConfig.endDate.day
-            && currentDate.getMonth() == eventConfig.endDate.month))
-        ) {
-            return true;
-        }
-    }
-    return false;
 }
 
 /**
@@ -96,30 +50,17 @@ function died(e) {
  */
 function init(e) {
     var player = e.player;
+    var activeEvents = getActiveEventList();
 
-    counter = 100;
+    if (activeEvents.length > 0 || player.getName() == "TheOddlySeagull") {
 
-    // Notify the player about active events
-    var activeEvents = [];
-    var currentDate = new Date();
-    for (var i = 0; i < allEventConfig.events.length; i++) {
-        var eventConfig = allEventConfig.events[i];
-        if (
-            (currentDate.getDate() > eventConfig.startDate.day
-            && currentDate.getMonth() == eventConfig.startDate.month)
-            || (currentDate.getDate() < eventConfig.endDate.day
-            && currentDate.getMonth() == eventConfig.endDate.month)) {
-                activeEvents.push(eventConfig.name);
+        if (activeEvents.length > 0) {
+            tellPlayer(player, "&6Active Events: &e" + activeEvents.join(", "));
         }
-    }
-
-    if (activeEvents.length > 0) {
-        tellPlayer(player, "&6Active Events: &e" + activeEvents.join(", "));
-    }
-
-
-    if (isEventActive("Easter Egg Hunt") || player.getName() == "TheOddlySeagull") {
-        var event_player_data = loadPlayerEventData("Easter Egg Hunt", player.getName());
+        
+        if (isEventActive("Easter Egg Hunt") || player.getName() == "TheOddlySeagull") {
+            spawnEasterStarterPack(player);
+        }
     }
 }
 
@@ -128,23 +69,25 @@ function init(e) {
  * @param {Object} e - The event object containing information about the tick event.
  */
 function tick(e) {
-    var player = e.player;
-    var playerName = player.getName();
-    // var currentTime = new Date().getTime();		
+    if (isAnyEventActive()) {
+        var player = e.player;
+        var playerName = player.getName();
+        // var currentTime = new Date().getTime();		
 
-    if (isEventActive("April Fools") && counter < 1) {
-        var event_player_data = loadPlayerEventData("April Fools", playerName);
-        playerLastSpawnTime = event_player_data.playerLastSpawnTime;
-        playerSpawnIntervals = event_player_data.playerSpawnIntervals;
-        // Check if it's time to spawn a new swarm (30 to 40 minutes interval)
-        var currentTime = new Date().getTime();
-        if (!playerLastSpawnTime || currentTime - playerLastSpawnTime > (playerSpawnIntervals || 30 * 60 * 1000)) {
-            run_aprilfools_event(player);
+        if (isEventActive("April Fools") && counter < 1) {
+            var event_player_data = loadPlayerEventData("April Fools", playerName);
+            playerLastSpawnTime = event_player_data.playerLastSpawnTime;
+            playerSpawnIntervals = event_player_data.playerSpawnIntervals;
+            // Check if it's time to spawn a new swarm (30 to 40 minutes interval)
+            var currentTime = new Date().getTime();
+            if (!playerLastSpawnTime || currentTime - playerLastSpawnTime > (playerSpawnIntervals || 30 * 60 * 1000)) {
+                run_aprilfools_event(player);
+            }
         }
-    }
 
-    if (counter > 0) {
-        counter--;
+        if (counter > 0) {
+            counter--;
+        }
     }
 }
 
@@ -162,7 +105,6 @@ function getRandomSpawnInterval() {
  * @param {Object} e - The event object containing information about the player leaving.
  */
 function logout(e) {
-    savePlayerSpawnData();
 
     if (isEventActive("April Fools")) {
         susbox_cleanup(e);
