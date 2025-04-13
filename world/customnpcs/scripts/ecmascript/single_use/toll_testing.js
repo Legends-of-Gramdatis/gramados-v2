fe1bdeded4bf85425d931d7b8f51e456f18826bf
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2025-04-13 22:45:22 +0200

    Add toll testing script with time reporting functionality

---
+++ world/customnpcs/scripts/ecmascript/single_use/toll_testing.js
@@ -0,0 +1,14 @@
+load("world/customnpcs/scripts/ecmascript/gramados_sounds/toll_sounds.js");
+load("world/customnpcs/scripts/ecmascript/gramados_utils/utils_chat.js");
+
+function tick(event) {
+    runToll(event);
+}
+
+function interact(event) {
+    var player = event.player;
+    var date = new Date();
+    tellPlayer(player, "Current time: " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
+    var world_time = event.player.getWorld().getTime()
+    tellPlayer(player, "World time: " + world_time);
+}
