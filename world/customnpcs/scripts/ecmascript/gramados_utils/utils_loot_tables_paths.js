ca75806f1131f6d6c8f8607229d6e1c27fb1a9d3
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2025-04-16 00:00:36 +0200

    Add chocolate loot table and Easter egg paths to loot table utils

--- world/customnpcs/scripts/ecmascript/gramados_utils/utils_loot_tables_paths.js
+++ world/customnpcs/scripts/ecmascript/gramados_utils/utils_loot_tables_paths.js
@@ -1,1 +1,3 @@
-var _LOOTTABLE_CHOCOLATE = "assembled_loot_tables/easter_eggs/spring_egg.json"
+var _LOOTTABLE_EASTEREGG_SPRING = "assembled_loot_tables/easter_eggs/spring_egg.json"
+var _LOOTTABLE_EASTEREGG_CHROMASHELL = "assembled_loot_tables/easter_eggs/chromashell_egg.json"
+var _LOOTTABLE_EASTEREGG_ENCRYPTED = "assembled_loot_tables/easter_eggs/chromashell_egg.json"
