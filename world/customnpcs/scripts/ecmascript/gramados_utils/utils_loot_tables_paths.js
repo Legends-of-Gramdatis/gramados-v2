c8daf35a3d4682203a55c74a82dbe2d572fad419
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2025-04-27 23:39:34 +0200

    Add vehicle bike and wrench loot table paths to utils

--- world/customnpcs/scripts/ecmascript/gramados_utils/utils_loot_tables_paths.js
+++ world/customnpcs/scripts/ecmascript/gramados_utils/utils_loot_tables_paths.js
@@ -7,3 +7,6 @@ var _LOOTTABLE_BANKVAULT_SAFE = "assembled_loot_tables/bank_safe.json"
 
 var _LOOTTABLE_NPCTYPE_ROBOT = "robots/robots_brokenbits.json"
 var _LOOTTABLE_NPCTYPE_HUMAN = "assembled_loot_tables/npc_human.json"
+
+var _LOOTTABLE_VEHICLE_BIKE = "automobile/vehicles/vehicle_ivl_bike.json";
+var _LOOTTABLE_VEHICLE_WRENCH = "tools/tools_mts_wrench.json";
