93e096319a5423cd358b95f31a74c671908e6258
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2025-07-19 11:04:02 +0200

    Fix bank vault gold rack loot table path

--- world/customnpcs/scripts/ecmascript/gramados_utils/utils_loot_tables_paths.js
+++ world/customnpcs/scripts/ecmascript/gramados_utils/utils_loot_tables_paths.js
@@ -2,7 +2,7 @@ var _LOOTTABLE_EASTEREGG_SPRING = "assembled_loot_tables/easter_eggs/spring_egg.
 var _LOOTTABLE_EASTEREGG_CHROMASHELL = "assembled_loot_tables/easter_eggs/chromashell_egg.json"
 var _LOOTTABLE_EASTEREGG_ENCRYPTED = "assembled_loot_tables/easter_eggs/chromashell_egg.json"
 
-var _LOOTTABLE_BANKVAULT_GOLDRACK = "treasures/treasures_gold_rack.json"
+var _LOOTTABLE_BANKVAULT_GOLDRACK = "treasures/treasures_ingots.json"
 var _LOOTTABLE_BANKVAULT_SAFE = "assembled_loot_tables/bank_safe.json"
 
 var _LOOTTABLE_NPCTYPE_ROBOT = "robots/robots_brokenbits.json"
