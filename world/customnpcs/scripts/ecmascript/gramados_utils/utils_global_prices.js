687aa527fcaee229f74eb6ec767442d7cf50825b
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2025-07-19 22:27:20 +0200

    Step 1: Centralification of JSON-Based pricing System Fixes #57 : Refactor getPrice function to include default price handling and improve NBT checks

--- world/customnpcs/scripts/ecmascript/gramados_utils/utils_global_prices.js
+++ world/customnpcs/scripts/ecmascript/gramados_utils/utils_global_prices.js
@@ -5,53 +5,67 @@ var STOCK_EXCHANGE_DATA_JSON_PATH = "world/customnpcs/scripts/globals/stock_exch
  * Gets the price of an item by its ID and NBT.
  * @param {string} itemId - The item ID.
  * @param {Object} itemTag - The item NBT.
+ * @param {number} defaultPrice - The default price to return if no price is found.
  * @returns {number} - The price of the item.
  */
-function getPrice(itemId, itemTag) {
-    var globalPrices = loadJson(GLOBAL_PRICES_JSON_PATH);
-    if (!globalPrices) {
-        return 0;
+function getPrice(itemId, defaultPrice, itemTag, ignoreNBT) {
+
+    // if item id doesn't fit with regex modid:itemid:damage (aka missing the :damage)
+    if (!/^.+:.+:\d+$/.test(itemId)) {
+        itemId += ":0";
     }
 
-    for (var i = 0; i < globalPrices.length; i++) {
-        if (globalPrices[i].id === itemId) {
-            if (globalPrices[i].tag) {
-                if (JSON.stringify(globalPrices[i].tag) === JSON.stringify(itemTag)) {
-                    if (globalPrices[i].stock_link) {
-                        var stockPrices = loadJson(STOCK_EXCHANGE_DATA_JSON_PATH);
-                        if (stockPrices[globalPrices[i].stock_link]) {
-                            return stockPrices[globalPrices[i].stock_link].current_price;
-                        }
-                    }
-                    return globalPrices[i].price;
+    var globalPrices = loadJson(GLOBAL_PRICES_JSON_PATH);
+
+    if (!globalPrices || !globalPrices[itemId]) {
+        return defaultPrice;
+    }
+
+    
+    var itemData = globalPrices[itemId];
+
+    if (itemData.tag && !ignoreNBT) {
+        if (JSON.stringify(itemData.tag) === JSON.stringify(itemTag)) {
+            if (itemData.stock_link) {
+                var stockPrices = loadJson(STOCK_EXCHANGE_DATA_JSON_PATH);
+                if (stockPrices[itemData.stock_link]) {
+                    return stockPrices[itemData.stock_link].current_price;
                 }
-            } else {
-                if (globalPrices[i].stock_link) {
-                    var stockPrices = loadJson(STOCK_EXCHANGE_DATA_JSON_PATH);
-                    if (stockPrices[globalPrices[i].stock_link]) {
-                        return stockPrices[globalPrices[i].stock_link].current_price;
-                    }
-                }
-                return globalPrices[i].price;
+            }
+            return itemData.value;
+        }
+    } else {
+        if (itemData.stock_link) {
+            var stockPrices = loadJson(STOCK_EXCHANGE_DATA_JSON_PATH);
+            if (stockPrices[itemData.stock_link]) {
+                return stockPrices[itemData.stock_link].current_price;
             }
         }
+        return itemData.value;
     }
 
-    return 0;
+    return defaultPrice;
 }
 
 /**
  * Gets the price of an item stack by its ID and NBT.
  * @param {IItemStack} stack - The item stack.
+ * @param {number} defaultPrice - The default price to return if no price is found.
+ * @param {boolean} ignoreNBT - Whether to ignore the NBT data when checking the price.
  * @returns {number} - The price of the item stack.
  */
-function getPriceFromItemStack(stack) {
-    var itemId = stack.getItemId();
-    var itemTag = stack.getItemNbt();
-    if (itemTag) {
-        itemTag = itemTag.getCompound("tag");
+function getPriceFromItemStack(stack, defaultPrice, ignoreNBT) {
+    var itemId = stack.getName();
+    if (stack.hasNbt()) {
+        var itemTag = stack.getNbt();
+        if (itemTag) {
+            itemTag = itemTag.getCompound("tag");
+        }
+    } else {
+        var itemTag = null; // No NBT data
     }
-    return getPrice(itemId, itemTag);
+    var value = getPrice(itemId, defaultPrice, itemTag, ignoreNBT);
+    return value;
 }
 
 /**
