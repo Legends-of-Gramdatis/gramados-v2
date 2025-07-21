d0eddcc234ec91cf2a3b2c5bb0f7e5c3588f0190
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2025-07-21 14:43:10 +0200

    Refactor crowbar pricing to use a variable and update purchase messages for consistency

--- world/customnpcs/scripts/ecmascript/modules/npc_scripts/junkyard_crate_manager.js
+++ world/customnpcs/scripts/ecmascript/modules/npc_scripts/junkyard_crate_manager.js
@@ -6,6 +6,8 @@ load('world/customnpcs/scripts/ecmascript/gramados_utils/utils_loot_tables_paths
 load("world/customnpcs/scripts/ecmascript/gramados_utils/utils_files.js");
 load("world/customnpcs/scripts/ecmascript/gramados_utils/utils_jobs.js");
 
+var crowbarPrice = 150000; // Price of the crowbar in grams
+
 
 function interact(event) {
     var npc = event.npc;
@@ -32,18 +34,18 @@ function interact(event) {
         return;
     }
 
-    if (getMoneyFromPlayerPouch(player, 150000)) {
+    if (getMoneyFromPlayerPouch(player, crowbarPrice)) {
         var loot = pullLootTable(_LOOTTABLE_JUNKYARD_CRATE_CROWBAR, player);
         var crowbar = setupCrowbarNameLore(loot[0], world);
         player.giveItem(crowbar);
         storytellPlayer(player, ["&7[&a✔&7] &fDeal's done. Here's your crowbar — one-use only, so make it count.","&8Check the crates behind the fence. You never know what you'll find."]);
-        logToFile("mechanics", playerName + " purchased a Junkyard Crate Crowbar for 1500g.");
+        logToFile("mechanics", playerName + " purchased a Junkyard Crate Crowbar for " + getAmountCoin(crowbarPrice));
 
         // Update the purchase data and save it
         purchaseData[playerName] = currentDay;
         saveJson(purchaseData, jsonFilePath);
     } else {
-        storytellPlayer(player, ["&7[&c✖&7] &fSorry, pal. &cThat crowbar costs 1500g.","&r&8Check your pouch and try again when you've got the funds."]);
+        storytellPlayer(player, ["&7[&c✖&7] &fSorry, pal. &cThat crowbar costs " + getAmountCoin(crowbarPrice) + ".","&r&8Check your pouch and try again when you've got the funds."]);
         return;
     }
 }
