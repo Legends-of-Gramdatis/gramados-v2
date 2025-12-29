87895c5bc2adc5b20758e8130f47b59dd216e537
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2025-12-29 21:52:01 +0100

    feat: enhance modifier recharge system with arcade token handling and sound effects

--- world/customnpcs/scripts/ecmascript/modules/modifiers/modifierEngine.js
+++ world/customnpcs/scripts/ecmascript/modules/modifiers/modifierEngine.js
@@ -5,6 +5,8 @@ load("world/customnpcs/scripts/ecmascript/gramados_utils/utils_currency.js");
 
 var MODIFIERS_CFG_PATH = "world/customnpcs/scripts/ecmascript/modules/modifiers/modifiers_config.json";
 
+var API = Java.type('noppes.npcs.api.NpcAPI').Instance();
+
 function interact(event) {
     var player = event.player;
     var item = player.getMainhandItem();
@@ -15,7 +17,7 @@ function interact(event) {
 
     var offItem = player.getOffhandItem();
 
-    if (!has_item_modifier_tag(item) && !offItem.isEmpty() && offItem.getName() == "mts:ivv.idcard_seagull") {
+    if (!offItem.isEmpty() && offItem.getName() == "mts:ivv.idcard_seagull" && !has_item_modifier_tag(item)) {
         var trace = player.rayTraceBlock(5, true, false);
         var block = trace.getBlock();
         if (block.getName() == "minecraft:chest") {
@@ -38,7 +40,6 @@ function interact(event) {
             var item_name = slotItem.getName();
             tellPlayer(player, "§e:recycle: Debug: Slot " + slot + ": " + item_name);
 
-            // First name tag found -> use its display name as modifier type
             if (!foundEntry && item_name === "minecraft:name_tag") {
                 var displayName = slotItem.getDisplayName();
                 tellPlayer(player, "§e:recycle: Debug: Found name tag with display name: " + displayName);
@@ -58,9 +59,9 @@ function interact(event) {
     if (has_item_modifier_tag(item)) {
         var trace = player.rayTraceBlock(5, true, false);
         var block = trace.getBlock();
+        var nbt = item.getItemNbt();
+        var tag = nbt.getCompound("tag");
         if (is_active_modifier_item(item) && block.getName() == "minecraft:air") {
-            var nbt = item.getItemNbt();
-            var tag = nbt.getCompound("tag");
             var modifierType = tag.getString("modifier_type");
             var radius = tag.getInteger("modifier_radius");
             tag.setBoolean("is_modifier", false);
@@ -80,12 +81,13 @@ function interact(event) {
                 curr_lore[0],
                 curr_lore[1],
                 ccs("&8This modifier has been used and needs to be recharged."),
-                ccs("&6Use the orb on a chest with §dArcade Token §6inside to recharge it.")
+                ccs("&6Use the orb on a chest with §dArcade Token §6inside to recharge it."),
+                ccs("&7Next token use count: §e" + (tag.getInteger("repairs")))
             ];
             usedItemStack.setLore(new_lore);
             player.setMainhandItem(usedItemStack);
-            // play broken glass sound
-            player.getWorld().playSoundAt(player.getPos(), "minecraft:block.glass.break", 1.0, 1.0);
+            var command = "/playsound customnpcs:magic.shot player @a " + player.getPos().getX() + " " + player.getPos().getY() + " " + player.getPos().getZ() + " 1 1";
+            API.executeCommand(player.getWorld(), command);
 
             return;
         } else if (!is_active_modifier_item(item)) {
@@ -99,7 +101,8 @@ function interact(event) {
             var slotCount = container.getSize();
             tellPlayer(player, "§e:recycle: Debug: Container has " + slotCount + " slots.");
 
-            var foundNameTag = false;
+            var tokens_counted = 0;
+            var token_slots = [];
             for (var slot = 0; slot < slotCount; slot++) {
                 var slotItem = container.getSlot(slot);
                 if (slotItem.isEmpty()) {
@@ -113,14 +116,66 @@ function interact(event) {
 
                 // Name tag found -> recharge modifier
                 if (isArcadeToken(newSlotItem)) {
-                    foundNameTag = true;
-                    newSlotItem.setStackSize(newSlotItem.getStackSize() - 1);
-                    container.setSlot(slot, newSlotItem);
-                    player.setMainhandItem(repair_modifier_item(player, player.getMainhandItem()));
-                    tellPlayer(player, "§a:recycle: Debug: Modifier recharged using an arcade token.");
+                    tokens_counted += newSlotItem.getStackSize();
+                    token_slots.push(slot);
+                }
+            }
+
+            if (tokens_counted == 0) {
+                tellPlayer(player, "§e:recycle: Debug: No arcade tokens found in the container to recharge the modifier.");
+                return;
+            }
+
+            // Use one token to recharge
+            var needed_tokens = tag.getInteger("repairs");
+            if (tokens_counted < needed_tokens) {
+                tellPlayer(player, "§e:recycle: Debug: Not enough arcade tokens to recharge the modifier. Needed: " + needed_tokens + ", found: " + tokens_counted);
+                return;
+            }
+
+            for (var i = 0; i < token_slots.length; i++) {
+                var slotIndex = token_slots[i];
+                var slotItem = container.getSlot(slotIndex);
+                var stackSize = slotItem.getStackSize();
+
+                if (stackSize <= needed_tokens) {
+                    // Remove entire stack
+                    container.setSlot(slotIndex, container.getEmptyItem());
+                    needed_tokens -= stackSize;
+                } else {
+                    // Remove partial stack
+                    var newStack = slotItem.copy();
+                    newStack.setStackSize(stackSize - needed_tokens);
+                    container.setSlot(slotIndex, newStack);
+                    needed_tokens = 0;
+                }
+
+                if (needed_tokens <= 0) {
                     break;
                 }
             }
+            var command = "/playsound customnpcs:magic.charge player @a " + player.getPos().getX() + " " + player.getPos().getY() + " " + player.getPos().getZ() + " 1 1";
+            API.executeCommand(player.getWorld(), command);
+
+            tellPlayer(player, "§a:recycle: Debug: Modifier recharged successfully using " + needed_tokens + " arcade tokens.");
+            
+            var curr_lore = item.getLore();
+            var new_lore = [
+                curr_lore[0],
+                curr_lore[1],
+                ccs("&7Next token use count: §e" + (tag.getInteger("repairs")))
+            ];
+            player.getMainhandItem().setLore(new_lore);
+
+            player.setMainhandItem(repair_modifier_item(player, item));
+
+            return;
         }
     }
 }
+
+function logout(e) {
+
+
+}
+
