681bab97f7daad7983a96958cb585c7877b97750
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2026-01-19 15:28:41 +0100

    feat: enhance guiBuilder_updateManifest to unlock buttons based on player conditions

--- world/customnpcs/scripts/ecmascript/modules/GUI_builder/guis/car_dealership/gui_car_dealership.js
+++ world/customnpcs/scripts/ecmascript/modules/GUI_builder/guis/car_dealership/gui_car_dealership.js
@@ -1,4 +1,5 @@
 load("world/customnpcs/scripts/ecmascript/gramados_utils/utils_jobs.js");
+load('world/customnpcs/scripts/ecmascript/gramados_utils/utils_currency.js');
 
 function guiButtons(event, buttonId, pageId) {
     switch (pageId) {
@@ -31,8 +32,17 @@ function guiButtons(event, buttonId, pageId) {
     }
 }
 
-function guiBuilder_updateManifest(event, manifest) {
+function  guiBuilder_updateManifest(event, manifest) {
     // Modify the manifest as needed before building the GUI
     // For example, if you want to make a locked button unlocked due to some conditions.
+    var player = event.player;
+    if (!player) return manifest;
+    tellPlayer(player, "Your nickname is: " + player.getDisplayName());
+    if (playerHasJobWithTag(player, "Mechanic")) {
+        manifest.pages[0].components[4].locked = false;
+    }
+    if (getMoneyInPouch(player) >= 1000000) {
+        manifest.pages[1].components[3].locked = false;
+    }
     return manifest;
 }
