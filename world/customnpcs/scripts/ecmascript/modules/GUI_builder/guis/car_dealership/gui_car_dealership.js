711e1b10108de4628224ef5f647ac3dca59776e5
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2026-01-19 12:23:47 +0100

    feat: add GUI components and logic for car dealership interface

---
+++ world/customnpcs/scripts/ecmascript/modules/GUI_builder/guis/car_dealership/gui_car_dealership.js
@@ -0,0 +1,30 @@
+function guiButtons(event, buttonId, pageId) {
+    switch (pageId) {
+        case 1:
+            switch (buttonId) {
+                // case 4:
+                //     // Do something
+                //     break;
+                // case 6:
+                //     // Do something
+                //     break;
+                // case 7:
+                //     // Do something
+                //     break;
+                // case 8:
+                //     // Do something
+                //     break;
+            }
+            break;
+        case 2:
+            switch (buttonId) {
+                // case 12:
+                //     // Do something
+                //     break;
+                // case 13:
+                //     // Do something
+                //     break;
+            }
+            break;
+    }
+}
