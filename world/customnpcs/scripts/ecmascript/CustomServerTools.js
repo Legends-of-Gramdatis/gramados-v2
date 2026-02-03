79c94cfbcaef5b010e386b6aa0c823be6e061df1
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2026-02-03 08:29:14 +0100

    feat: using util_files in main OG script

--- world/customnpcs/scripts/ecmascript/CustomServerTools.js
+++ world/customnpcs/scripts/ecmascript/CustomServerTools.js
@@ -1,3 +1,5 @@
+load('world/customnpcs/scripts/ecmascript/gramados_utils/utils_files.js');
+
 /*
     Custom Server Tools
 */
@@ -2131,37 +2133,6 @@ var Files = Java.type("java.nio.file.Files");
 var Paths = Java.type("java.nio.file.Paths");
 var CHARSET_UTF_8 = Java.type("java.nio.charset.StandardCharsets").UTF_8;
 
-
-function mkPath(path) {
-    var expath = path.split("/");
-    var curpath = "";
-    for (var ex in expath) {
-        var expt = expath[ex];
-        curpath += (curpath == "" ? "" : "/") + expt;
-        var pfile = new File(curpath);
-        if (!pfile.exists()) {
-            if (expt.match(/[\w]+\.[\w]+/) === null) { //is dir?
-                pfile.mkdir();
-            } else {
-                pfile.createNewFile();
-            }
-        }
-    }
-}
-
-function readDir(dirPath) {
-    var res = [];
-    var files = new File(dirPath).listFiles();
-    for (var id in files) {
-        var file = files[id];
-        if (file.isDirectory())
-            res = res.concat(readDir(file.toString()));
-        else
-            res.push(Java.from(readFile(file.toString())).join("\n").replace(/\t/g, "  "));
-    }
-    return res;
-}
-
 function readFileAsString(filePath) {
     try {
         return Java.from(readFile(filePath)).join("\n").replace(/\t/g, "  ");
@@ -2181,22 +2152,6 @@ function readFile(filePath) {
         return [];
     }
 }
-
-function writeToFile(filePath, text, offset, length) {
-    if (typeof (offset) == typeof (undefined) || offset === null) { offset = null; }
-    if (typeof (length) == typeof (undefined) || length === null) { length = null; }
-    var path = Paths.get(filePath);
-    try {
-        var writer = Files.newBufferedWriter(path, CHARSET_UTF_8);
-        writer.write(text, offset || 0, length || text.length);
-        writer.close();
-        return true;
-    } catch (exc) {
-        return false
-    }
-}
-
-
 
 // Onboarding command-run logger (minimal, self-contained; avoids loading external utils)
 // Updates world/customnpcs/scripts/data_auto/onboarding_data.json when:
