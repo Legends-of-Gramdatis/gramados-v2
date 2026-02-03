43a97a6807b0537f9ac7c7537dc34dab4525570e
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2026-02-03 09:10:01 +0100

    feat: using util_emotes in main OG script

--- world/customnpcs/scripts/ecmascript/CustomServerTools.js
+++ world/customnpcs/scripts/ecmascript/CustomServerTools.js
@@ -3,6 +3,7 @@ load('world/customnpcs/scripts/ecmascript/gramados_utils/utils_general.js');
 load('world/customnpcs/scripts/ecmascript/gramados_utils/utils_maths.js');
 load('world/customnpcs/scripts/ecmascript/gramados_utils/utils_chat.js');
 load('world/customnpcs/scripts/ecmascript/gramados_utils/utils_currency.js');
+load('world/customnpcs/scripts/ecmascript/gramados_utils/utils_emotes.js');
 
 var _RAWCOLORS = {
     '0': 'black',
@@ -403,61 +404,6 @@ var CSTENCH_TAG = "CSTEnch";
 /*
     Custom Server Tools
 */
-
-function grantEmote(player, emote) {
-    if (giveEmote(player, emote)) {
-        tellPlayer(player, "&a:check_mark: You have received the '&r:" + emote + ":&a' emote!&8&o Use !myemotes to see your emotes.");
-        var command = "/playsound minecraft:entity.player.levelup block @a " + player.getPosition().getX() + " " + player.getPosition().getY() + " " + player.getPosition().getZ() + " 1 1";
-        API.executeCommand(player.getWorld(), command);
-    }
-}
-
-function giveEmote(player, emote) {
-    var world_data = player.getWorld().getStoreddata();
-    var player_json = JSON.parse(world_data.get("player_" + player.getDisplayName()));
-
-    if (!player_json) {
-        player_json = {};
-    }
-
-    player_json.emotes = player_json.emotes || [];
-
-    if (!includes(player_json.emotes, emote)) {
-        player_json.emotes.push(emote);
-        world_data.put("player_" + player.getDisplayName(), JSON.stringify(player_json));
-        return true;
-    }
-    return false;
-}
-
-function grantBadgeAndEmotes(player, badge, emotes) {
-    for (var i = 0; i < emotes.length; i++) {
-        giveEmote(player, emotes[i]);
-    }
-    if (giveBadge(player, badge)) {
-        tellPlayer(player, "&a:check_mark: You have received the '&r" + badge + "&a' badge!&8&o Use !mybadges to see your badges.");
-        var command = "/playsound minecraft:entity.player.levelup block @a " + player.getPos().getX() + " " + player.getPos().getY() + " " + player.getPos().getZ() + " 1 1";
-        API.executeCommand(player.getWorld(), command);
-    }
-}
-
-function giveBadge(player, badge) {
-    var world_data = player.getWorld().getStoreddata();
-    var player_json = JSON.parse(world_data.get("player_" + player.getDisplayName()));
-
-    if (!player_json) {
-        player_json = {};
-    }
-
-    player_json.badges = player_json.badges || [];
-
-    if (!includes(player_json.badges, badge)) {
-        player_json.badges.push(badge);
-        world_data.put("player_" + player.getDisplayName(), JSON.stringify(player_json));
-        return true;
-    }
-    return false;
-}
 
 Date.prototype.addTime = function (addTime) {
     this.setTime(this.getTime() + addTime);
