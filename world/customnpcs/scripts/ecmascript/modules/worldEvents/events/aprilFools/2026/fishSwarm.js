3c6af0486c370095c274709d79d2b174c6593997
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2026-03-18 21:18:22 +0100

    feat: implement run_aprilfools_event function to handle fish swarm spawning

--- world/customnpcs/scripts/ecmascript/modules/worldEvents/events/aprilFools/2026/fishSwarm.js
+++ world/customnpcs/scripts/ecmascript/modules/worldEvents/events/aprilFools/2026/fishSwarm.js
@@ -3,6 +3,15 @@ load("world/customnpcs/scripts/ecmascript/gramados_utils/utils_spawning.js");
 
 var fishCloneName = "AprilFools Fish Rain";
 
+function run_aprilfools_event(player) {
+    var playerName = player.getName();
+    var success_spawns = spawnFishSwarm(player, 5, 3);
+    var logline = "Spawned " + success_spawns + " Fish Rain clones around " + playerName + ".";
+    logToFile("events", logline);
+
+    playerLastSpawnTime[playerName] = new Date().getTime();
+    playerSpawnIntervals[playerName] = getRandomSpawnInterval();
+}
 
 function spawnFishSwarm(player, size, radius) {
     var world = player.getWorld();
