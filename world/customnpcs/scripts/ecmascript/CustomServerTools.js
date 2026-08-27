0c650a18acc92042e319358292a6cac77f01e14b
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2026-08-27 18:44:15 +0200

    feat: merged test scripts into main script

--- world/customnpcs/scripts/ecmascript/CustomServerTools.js
+++ world/customnpcs/scripts/ecmascript/CustomServerTools.js
@@ -11,6 +11,7 @@ load('world/customnpcs/scripts/ecmascript/gramados_utils/utils_logging.js');
 load('world/customnpcs/scripts/ecmascript/gramados_utils/utils_date.js');
 load('world/customnpcs/scripts/ecmascript/gramados_utils/utils_region.js');
 load('world/customnpcs/scripts/ecmascript/gramados_utils/utils_winemaking.js');
+load('world/customnpcs/scripts/ecmascript/gramados_utils/utils_volatile_loot_pools.js');
 load('world/customnpcs/scripts/ecmascript/modules/worldEvents/worldEventUtils.js');
 load('world/customnpcs/scripts/ecmascript/modules/contactInformation/contactInformation_utils.js');
 
@@ -4028,6 +4029,87 @@ registerXCommands([
         return true;
     }, 'item.value'],
 
+]);
+
+registerXCommands([
+    ['!loottableVLP add <alias> <weight>', function (pl, args, data) {
+        var mainhand = pl.getMainhandItem();
+
+        if (!mainhand || mainhand.isEmpty()) {
+            tellPlayer(pl, '&c:cross: Hold the item to add in your main hand.');
+            return false;
+        }
+
+        var submittedItem = mainhand.copy();
+        var result = addItemStackToVolatileLootPool(
+            args.alias,
+            submittedItem,
+            args.weight
+        );
+
+        if (!result.Success) {
+            switch (result.Reason) {
+                case 'unknown_alias':
+                    tellPlayer(pl, '&c:cross: Unknown volatile loot pool alias: &e' + args.alias + '&c.');
+                    break;
+                case 'invalid_weight':
+                    tellPlayer(pl, '&c:cross: Loot-table weight must be greater than 0.');
+                    break;
+                case 'not_whitelisted':
+                    tellPlayer(pl, '&c:cross: This item is not allowed in volatile loot pool &e' + result.Alias + '&c.');
+                    break;
+                case 'invalid_whitelist':
+                    tellPlayer(pl, '&c:cross: Volatile loot pool &e' + result.Alias + '&c has an invalid whitelist configuration.');
+                    break;
+                case 'write_failed':
+                    tellPlayer(pl, '&c:cross: Failed to add the item to volatile loot pool &e' + result.Alias + '&c. The item was not consumed.');
+                    break;
+                default:
+                    tellPlayer(pl, '&c:cross: Failed to add the item to the volatile loot pool. The item was not consumed.');
+                    break;
+            }
+
+            return false;
+        }
+
+        // Consume exactly one item only after the loot-table write succeeded.
+        var remaining = mainhand.getStackSize() - 1;
+
+        if (remaining <= 0) {
+            pl.setMainhandItem(pl.getWorld().createItem('minecraft:air', 0, 1));
+        } else {
+            var remainingStack = mainhand.copy();
+            remainingStack.setStackSize(remaining);
+            pl.setMainhandItem(remainingStack);
+        }
+
+        var itemName = stripColors(submittedItem.getDisplayName());
+
+        tellPlayer(
+            pl,
+            '&a:check_mark: Added &e' + submittedItem.getDisplayName() +
+            '&a to volatile loot pool &e' + result.Alias +
+            '&a with weight &e' + args.weight + '&a.'
+        );
+
+        logToFile(
+            'loot_tables',
+            '[VLP] ' + pl.getName() + " added '" + itemName +
+            "' to '" + result.Alias + "' (weight " + args.weight + ').'
+        );
+
+        return true;
+    }, 'loottableVLP.add', [
+        {
+            'argname': 'alias',
+            'type': 'string'
+        },
+        {
+            'argname': 'weight',
+            'type': 'number',
+            'min': 0.000001
+        }
+    ]]
 ]);
 
 registerXCommands([
