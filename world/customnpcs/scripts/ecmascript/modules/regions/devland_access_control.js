2137126a5841607fe7d67acaa07da1664bd6fc3f
TheOddlySeagull <TheOddlySeagull00@gmail.com>
2026-09-11 00:26:01 +0200

    feat: added a whitelist to Devland region.

--- world/customnpcs/scripts/ecmascript/modules/regions/devland_access_control.js
+++ world/customnpcs/scripts/ecmascript/modules/regions/devland_access_control.js
@@ -6,6 +6,11 @@ var DEVLAND_REGION = 'Devland';
 function tick(event) {
     var player = event.player;
     if (!player) return;
+    // if player name in list, return
+    var excludedPlayers = ['TheOddlySeagull', 'Peter_Gramados'];
+    if (includes(excludedPlayers, player.name)) {
+        return;
+    }
 
     if (excludePlayerFromRegion(player, DEVLAND_REGION)) {
         tellPlayer(player, "&c:cross_mark: Devland is a restricted development area. If you'd like a tour, ask a staff member and they can show you around.");
